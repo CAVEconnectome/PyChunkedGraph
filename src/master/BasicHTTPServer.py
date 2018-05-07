afec1fab008091b76fe372ce6ecb80c8e4b3164b
Zoe A <zashwood@princeton.edu>
2018-05-07 13:34:09 -0400

    Updated server code for python3

--- src/master/BasicHTTPServer.py
+++ src/master/BasicHTTPServer.py
@@ -16,8 +16,8 @@ Send a HEAD request via the terminal (assuming server running locally on port 91
 Send a POST request (assuming server running locally on port 9100)::
     curl -d "foo=bar&bin=baz" --insecure -i https://localhost:9100/1.0/graph/split/?
 """
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-import SocketServer
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import socketserver
 import json #To parse JSON system configuration file
 import ssl #For SSL encryption
 import os #To make directories as appropriate and to execute terminal commands
@@ -36,64 +36,75 @@ class S(BaseHTTPRequestHandler):
 
     # Handle HTTP GET requests  
     def do_GET(self):
-    	# handle each type of HTTP GET request. NB self.path is request
-    	# Get root request:
-    	if(re.match(r"/1.0/segment/(\d+)/root/?", self.path)):
-    		print "Get root request received" #Return root ID as body of HTTP response
-        	self._set_headers()
-        	self.send_response(200, "Test response")
-        	#self.wfile.write("<html><body><h1>hi!</h1></body></html>")
+        # handle each type of HTTP GET request. NB self.path is request
+        # Get root request:
+        if(re.match(r"/1.0/segment/(\d+)/root/?", self.path)):
+            print("Get root request received") #Return root ID as body of HTTP response
+            self._set_headers()
+            self.send_response(200, "Test response")
+            self.end_headers()
+            out_str = "<html><body><h1>Hello!</h1></body></html>"
+            self.wfile.write(out_str.encode("utf-8"))
+            #self.wfile.write("<html><body><h1>hi!</h1></body></html>")
         # Get active operations (items in write queue)
-        elif(re.match(r"/1.0/graph/active/?", self.path)):	
-        	print "Get active operations request received"
-        	# Return list of active write operations
-        	self._set_headers()
-        	self.wfile.write("<html><body><h1>hi!</h1></body></html>")
+        elif(re.match(r"/1.0/graph/active/?", self.path)):
+            print("Get active operations request received")
+            # Return list of active write operations
+            self._set_headers()
+            self.end_headers()
+            #self.wfile.write("<html><body><h1>hi!</h1></body></html>")
         else:
-        	print "could not parse " + self.path
-        	self._set_headers()
-        	# TODO: return error message
+            print("could not parse " + self.path)
+            self._set_headers()
+            self.end_headers()
+            # TODO: return error message
 
      
     # Handle merge and split operations by appending these to an outside DB    
     def do_POST(self):
         # handle each type of HTTP POST request. NB self.path is request
-    	# Merge supervoxels:
-    	if(re.match(r"/1.0/graph/merge/?", self.path)):
-    		print "Merge request received" #Return root ID as body of HTTP response
-        	self._set_headers()
-        	self.wfile.write("<html><body><h1>hi!</h1></body></html>")
+        # Merge supervoxels:
+        if(re.match(r"/1.0/graph/merge/?", self.path)):
+            print("Merge request received") #Return root ID as body of HTTP response
+            self._set_headers()
+            self.end_headers()
+            #self.wfile.write("<html><body><h1>hi!</h1></body></html>")
         # Handle split
         elif(re.match(r"/1.0/graph/split/?", self.path)):
-    		print "Split request received" #Return root ID as body of HTTP response
-        	self._set_headers()
-        	self.wfile.write("<html><body><h1>hi!</h1></body></html>")	
-        # Subscribe edges request:	
+            print("Split request received") #Return root ID as body of HTTP response
+            self._set_headers()
+            self.end_headers()
+            #self.wfile.write("<html><body><h1>hi!</h1></body></html>")
+        # Subscribe edges request:
         elif(re.match(r"/1.0/segment/(\d+)/subscribe/?", self.path)):
-        	print "Subscribe edges request received"
-        	self._set_headers()
-        	self.wfile.write("<html><body><h1>hi!</h1></body></html>")
-        # Unsubscribe edges request:		
+            print("Subscribe edges request received")
+            self._set_headers()
+            self.end_headers()
+            #self.wfile.write("<html><body><h1>hi!</h1></body></html>")
+        # Unsubscribe edges request:
         elif(re.match(r"/1.0/segment/(\d+)/unsubscribe/?", self.path)):	
-        	print "Unsubscribe edges request received"
-        	self._set_headers()
-        	self.wfile.write("<html><body><h1>hi!</h1></body></html>")
-        # Save graph	
-        elif(re.match(r"/1.0/graph/save/?", self.path)):	
-        	print "Save request received"
-        	self._set_headers()
-        	self.wfile.write("<html><body><h1>hi!</h1></body></html>")
+            print("Unsubscribe edges request received")
+            self._set_headers()
+            self.end_headers()
+            #self.wfile.write("<html><body><h1>hi!</h1></body></html>")
+        # Save graph
+        elif(re.match(r"/1.0/graph/save/?", self.path)):
+            print("Save request received")
+            self._set_headers()
+            self.end_headers()
+            #self.wfile.write("<html><body><h1>hi!</h1></body></html>")
         else:
-        	print "could not parse " + self.path
-        	self._set_headers()
-        	#TODO: return error message			
+            print("could not parse " + self.path)
+            self._set_headers()
+            self.end_headers()
+            #TODO: return error message
         
 def run(certfile, server_class=HTTPServer, handler_class=S, port=9100):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     #Wrap socket in SSL context
     httpd.socket = ssl.wrap_socket (httpd.socket, certfile = certfile, server_side=True)
-    print 'Starting httpd...'
+    print('Starting httpd on port ' + str(port) + '...')
     httpd.serve_forever()
 
 if __name__ == "__main__":
