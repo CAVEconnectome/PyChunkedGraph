d2dcd5b1ea01b5d67566d4f93bd188a6e04b2f43
sdorkenw <sven.dorkenwald@googlemail.com>
2018-05-26 20:53:03 -0400

    added testing scenarios

--- src/pychunkedgraph/chunkedgraphserver_test.py
+++ src/pychunkedgraph/chunkedgraphserver_test.py
@@ -2,14 +2,15 @@ import requests
 import time
 import json
 
+IP = 0
 
 def request(op, body, post=True):
 
     if post:
-        url = 'https://10.240.0.50:4000/1.0/segment/{0}/{1}/'.format(body[0], op)
+        url = 'https://{0}:4000/1.0/segment/{1}/{2}'.format(IP, body[0], op)
         body = []
     else:
-        url = 'https://10.240.0.50:4000/1.0/graph/{0}/'.format(op)
+        url = 'https://{0}:4000/1.0/graph/{1}'.format(IP, op)
 
     print(url)
     time_start = time.time()
@@ -31,11 +32,31 @@ def get_root(atomic_id):
     return r
 
 
-def get_subgraph(atomic_id):
+def get_children(parent_id):
+    body = [str(parent_id), 0, 0, 0]
+
+    print(body)
+    r = request("children", body, post=True)
+
+    # print(r.content)
+    return r
+
+
+def get_leaves(atomic_id):
     body = [str(atomic_id), 0, 0, 0]
 
     print(body)
     r = request("leaves", body, post=True)
+
+    # print(r.content)
+    return r
+
+
+def get_leaves_from_leave(atomic_id):
+    body = [str(atomic_id), 0, 0, 0]
+
+    print(body)
+    r = request("leaves_from_leave", body, post=True)
 
     # print(r.content)
     return r
