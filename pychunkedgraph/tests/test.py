1eb4530b40bf347c01431e4937f5a71ff96137d8
Nico Kemnitz <nkemnitz@princeton.edu>
2018-06-05 23:32:08 -0400

    unit tests: Templates for Split and Merge Tests!

--- pychunkedgraph/tests/test.py
+++ pychunkedgraph/tests/test.py
@@ -43,18 +43,20 @@ def cgraph(request):
     return graph
 
 
-class TestChunkedGraph:
+class TestCoreUtils:
     def test_serialize_valid_node_id(self):
         assert chunkedgraph.serialize_node_id(0x01102001000000FF) == b'00076596382332354815'
 
-    def test_add_single_node(self, cgraph):
+
+class TestBuildGraph:
+    def test_build_single_node(self, cgraph):
         """
         Add single RG node 1 to chunk A
-        +-----+
-        |  A  |
-        |  1  |
-        |     |
-        +-----+
+        ┌─────┐
+        │  A¹ │
+        │  1  │
+        │     │
+        └─────┘
         """
 
         # Add Chunk A
@@ -106,14 +108,14 @@ class TestChunkedGraph:
         # Make sure there are not any more entries in the table
         assert len(res.rows) == 3
 
-    def test_add_single_edge(self, cgraph):
+    def test_build_single_edge(self, cgraph):
         """
-        Add edge between RG supervoxels 1 and 2 (same chunk)
-        +-----+
-        |  A  |
-        | 1-2 |
-        |     |
-        +-----+
+        Create graph with edge between RG supervoxels 1 and 2 (same chunk)
+        ┌─────┐
+        │  A¹ │
+        │ 1━2 │
+        │     │
+        └─────┘
         """
 
         # Add Chunk A
@@ -181,14 +183,14 @@ class TestChunkedGraph:
         # Make sure there are not any more entries in the table
         assert len(res.rows) == 5
 
-    def test_add_single_across_edge(self, cgraph):
+    def test_build_single_across_edge(self, cgraph):
         """
-        Add edge between RG supervoxels 1 and 2 (neighboring chunks)
-        +-----+-----+
-        |  A  |  B  |
-        |   1-|-2   |
-        |     |     |
-        +-----+-----+
+        Create graph with edge between RG supervoxels 1 and 2 (neighboring chunks)
+        ┌─────┌─────┐
+        │  A¹ │  B¹ │
+        │  1━━┿━━2  │
+        │     │     │
+        └─────┴─────┘
         """
 
         # Chunk A
@@ -294,15 +296,15 @@ class TestChunkedGraph:
         # Make sure there are not any more entries in the table
         assert len(res.rows) == 7
 
-    def test_add_single_edge_and_single_across_edge(self, cgraph):
+    def test_build_single_edge_and_single_across_edge(self, cgraph):
         """
-        Add edge between RG supervoxels 1 and 2 (same chunk)
-        Add edge between RG supervoxels 1 and 3 (neighboring chunks)
-        +-----+-----+
-        |  A  |  B  |
-        | 2-1-|-3   |
-        |     |     |
-        +-----+-----+
+        Create graph with edge between RG supervoxels 1 and 2 (same chunk)
+        and edge between RG supervoxels 1 and 3 (neighboring chunks)
+        ┌─────┬─────┐
+        │  A¹ │  B¹ │
+        │ 2━1━┿━━3  │
+        │     │     │
+        └─────┴─────┘
         """
 
         # Chunk A
@@ -428,3 +430,166 @@ class TestChunkedGraph:
 
         # Make sure there are not any more entries in the table
         assert len(res.rows) == 9
+
+
+class TestGraphMerge:
+    def test_merge_pair_same_chunk(self, cgraph):
+        """
+        Add edge between existing RG supervoxels 1 and 2 (same chunk)
+        ┌─────┐      ┌─────┐
+        │  A¹ │      │  A¹ │
+        │ 1 2 │  =>  │ 1━2 │
+        │     │      │     │
+        └─────┘      └─────┘
+        """
+
+        pass
+
+    def test_merge_pair_neighboring_chunks(self, cgraph):
+        """
+        Add edge between existing RG supervoxels 1 and 2 (neighboring chunks)
+        ┌─────┬─────┐      ┌─────┬─────┐
+        │  A¹ │  B¹ │      │  A¹ │  B¹ │
+        │  1  │  2  │  =>  │  1━━┿━━2  │
+        │     │     │      │     │     │
+        └─────┴─────┘      └─────┴─────┘
+        """
+
+        pass
+
+    def test_merge_pair_disconnected_chunks(self, cgraph):
+        """
+        Add edge between existing RG supervoxels 1 and 2 (disconnected chunks)
+        ┌─────┐     ┌─────┐      ┌─────┐     ┌─────┐
+        │  A¹ │ ... │  Z¹ │      │  A¹ │ ... │  Z¹ │
+        │  1  │     │  2  │  =>  │  1━━┿━━━━━┿━━2  │
+        │     │     │     │      │     │     │     │
+        └─────┘     └─────┘      └─────┘     └─────┘
+        """
+
+        pass
+
+    def test_merge_pair_already_connected(self, cgraph):
+        """
+        Add edge between already connected RG supervoxels 1 and 2 (same chunk)
+        ┌─────┐      ┌─────┐
+        │  A¹ │      │  A¹ │
+        │ 1━2 │  =>  │ 1━2 │
+        │     │      │     │
+        └─────┘      └─────┘
+        """
+
+        # Preparation: Build Chunk A
+        cg2rg = {0x0100000000000000: 1, 0x0100000000000001: 2}
+        rg2cg = {v: k for k, v in cg2rg.items()}
+
+        edge_ids = np.array([[0x0100000000000000, 0x0100000000000001]], dtype=np.uint64)
+        edge_affs = np.array([0.5], dtype=np.float32, ndmin=2)
+
+        cross_edge_ids = np.empty((0, 2), dtype=np.uint64)
+        cross_edge_affs = np.empty((0, 1), dtype=np.float32)
+
+        cgraph.add_atomic_edges_in_chunks(edge_ids, cross_edge_ids,
+                                          edge_affs, cross_edge_affs,
+                                          cg2rg, rg2cg)
+
+        # Merge
+        assert cgraph.add_edge([0x0100000000000001, 0x0100000000000000], is_cg_id=True) == 0x020000000
+
+        # Check
+        res = cgraph.table.read_rows()
+        res.consume_all()
+
+    def test_merge_triple_chain_to_full_circle_same_chunk(self, cgraph):
+        """
+        Add edge between indirectly connected RG supervoxels 1 and 2 (same chunk)
+        ┌─────┐      ┌─────┐
+        │  A¹ │      │  A¹ │
+        │ 1 2 │  =>  │ 1━2 │
+        │ ┗3┛ │      │ ┗3┛ │
+        └─────┘      └─────┘
+        """
+        pass
+
+    def test_merge_triple_chain_to_full_circle_neighboring_chunks(self, cgraph):
+        """
+        Add edge between indirectly connected RG supervoxels 1 and 2 (neighboring chunks)
+        ┌─────┬─────┐      ┌─────┬─────┐
+        │  A¹ │  B¹ │      │  A¹ │  B¹ │
+        │  1  │  2  │  =>  │  1━━┿━━2  │
+        │  ┗3━┿━━┛  │      │  ┗3━┿━━┛  │
+        └─────┴─────┘      └─────┴─────┘
+        """
+        pass
+
+    def test_merge_triple_chain_to_full_circle_disconnected_chunks(self, cgraph):
+        """
+        Add edge between indirectly connected RG supervoxels 1 and 2 (disconnected chunks)
+        ┌─────┐     ┌─────┐      ┌─────┐     ┌─────┐
+        │  A¹ │ ... │  Z¹ │      │  A¹ │ ... │  Z¹ │
+        │  1  │     │  2  │  =>  │  1━━┿━━━━━┿━━2  │
+        │  ┗3━┿━━━━━┿━━┛  │      │  ┗3━┿━━━━━┿━━┛  │
+        └─────┘     └─────┘      └─────┘     └─────┘
+        """
+        pass
+
+    def test_merge_pair_abstract_nodes(self, cgraph):  # This should fail
+        """
+        Add edge between RG supervoxel 1 and abstract node "2".
+                     ┌─────┐
+                     │  B² │
+                     │ "2" │
+                     │     │
+                     └─────┘
+        ┌─────┐
+        │  A¹ │
+        │  1  │
+        │     │
+        └─────┘
+        """
+        pass
+
+
+class TestGraphSplit:
+    def test_split_pair_same_chunk(self, cgraph):
+        pass
+
+    def test_split_pair_neighboring_chunks(self, cgraph):
+        pass
+
+    def test_split_pair_disconnected_chunks(self, cgraph):
+        pass
+
+    def test_split_pair_already_disconnected(self, cgraph):
+        pass
+
+    def test_split_full_circle_to_triple_chain_same_chunk(self, cgraph):
+        pass
+
+    def test_split_full_circle_to_triple_chain_neighboring_chunks(self, cgraph):
+        pass
+
+    def test_split_full_circle_to_triple_chain_disconnected_chunks(self, cgraph):
+        pass
+
+    def test_split_pair_abstract_nodes(self, cgraph):  # This should fail
+        pass
+
+
+class TestGraphMinCut:
+    def test_cut_low_affinity_chain(self, cgraph):
+        pass
+
+    def test_cut_zero_affinity_chain(self, cgraph):
+        pass
+
+    def test_cut_inf_affinity_chain(self, cgraph):
+        pass
+
+    def test_cut_no_path(self, cgraph):
+        pass
+
+
+class TestGraphMultiCut:
+    def test_cut_multi_tree(self, cgraph):
+        pass
