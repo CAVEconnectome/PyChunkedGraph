59009491d499638295e603e6d6a1886ee34f03b3
sdorkenw <sven.dorkenwald@googlemail.com>
2018-06-27 12:36:31 -0700

    added image rewriting for recubing image blocks

--- pychunkedgraph/backend/dataset_transformer.py
+++ pychunkedgraph/backend/dataset_transformer.py
@@ -1,3 +1,4 @@
+import itertools
 import numpy as np
 import glob
 import os
@@ -24,13 +25,14 @@ def _rewrite_segmentation_thread(args):
         if i_fp % 10 == 5:
             dt = time.time() - time_start
             eta = dt / i_fp * n_file_paths - dt
-            print("%d / %d - dt: %.3fs - eta: %.3fs" % (i_fp, n_file_paths, dt, eta))
+            print("%d / %d - dt: %.3fs - eta: %.3fs" %
+                  (i_fp, n_file_paths, dt, eta))
 
-        rewrite_single_block(fp, from_cv=from_cv, to_cv=to_cv)
+        rewrite_single_segmentation_block(fp, from_cv=from_cv, to_cv=to_cv)
 
 
-def rewrite_single_block(file_path, from_cv=None, to_cv=None, from_url=None,
-                         to_url=None):
+def rewrite_single_segmentation_block(file_path, from_cv=None, to_cv=None,
+                                      from_url=None, to_url=None):
     if from_cv is None:
         assert from_url is not None
         from_cv = cloudvolume.CloudVolume(from_url)
@@ -101,4 +103,102 @@ def rewrite_segmentation(dataset_name, n_threads=64, n_units_per_thread=None):
                              debug=n_threads == 1)
     else:
         mu.multisubprocess_func(_rewrite_segmentation_thread, multi_args,
+                                n_threads=n_threads)
+
+
+def _rewrite_image_thread(args):
+    start_coordinates, end_coordinates, block_size, from_url, to_url, mip = args
+
+    from_cv = cloudvolume.CloudVolume(from_url, mip=mip)
+    to_cv = cloudvolume.CloudVolume(to_url, bounded=False, mip=mip)
+
+    assert 'svenmd' in to_url
+
+    coordinate_iter = itertools.product(np.arange(start_coordinates[0], end_coordinates[0], block_size[0]),
+                                        np.arange(start_coordinates[1], end_coordinates[1], block_size[1]),
+                                        np.arange(start_coordinates[2], end_coordinates[2], block_size[2]))
+
+    for coordinate in coordinate_iter:
+        rewrite_single_image_block(coordinate, block_size, from_cv=from_cv,
+                                   to_cv=to_cv)
+
+
+def rewrite_single_image_block(coordinate, block_size, from_cv=None, to_cv=None,
+                               from_url=None, to_url=None, mip=None):
+    if from_cv is None:
+        assert from_url is not None and mip is not None
+        from_cv = cloudvolume.CloudVolume(from_url, mip=mip)
+
+    if to_cv is None:
+        assert to_url is not None and mip is not None
+        assert 'svenmd' in to_url
+        to_cv = cloudvolume.CloudVolume(to_url, bounded=False, mip=mip)
+
+    x_start = coordinate[0]
+    x_end = coordinate[0] + block_size[0]
+    y_start = coordinate[1]
+    y_end = coordinate[1] + block_size[1]
+    z_start = coordinate[2]
+    z_end = coordinate[2] + block_size[2]
+
+    bbox = to_cv.bounds.to_list()[3:]
+    if x_end > bbox[0]:
+        x_end = bbox[0]
+
+    if y_end > bbox[1]:
+        y_end = bbox[1]
+
+    if z_end > bbox[2]:
+        z_end = bbox[2]
+
+    print(x_start, y_start, z_start, x_end, y_end, z_end)
+
+    img = from_cv[x_start: x_end, y_start: y_end, z_start: z_end]
+    to_cv[x_start: x_end, y_start: y_end, z_start: z_end] = img
+
+
+def rewrite_image(dataset_name, block_size=(512, 512, 64), n_threads=64,
+                  mip=0):
+    if dataset_name == "pinky":
+        from_url = "gs://neuroglancer/pinky40_v11/image_rechunked/"
+        to_url = "gs://neuroglancer/svenmd/pinky40_v11/image_256_256_8/"
+    elif dataset_name == "basil":
+        raise()
+    else:
+        raise Exception("Dataset unknown")
+
+    from_cv = cloudvolume.CloudVolume(from_url, mip=mip)
+
+    dataset_bounds = np.array(from_cv.bounds.to_list())
+    block_size = np.array(list(block_size))
+
+    super_block_size = block_size * 2
+
+    coordinate_iter = itertools.product(np.arange(dataset_bounds[0],
+                                                  dataset_bounds[3],
+                                                  super_block_size[0]),
+                                        np.arange(dataset_bounds[1],
+                                                  dataset_bounds[4],
+                                                  super_block_size[1]),
+                                        np.arange(dataset_bounds[2],
+                                                  dataset_bounds[5],
+                                                  super_block_size[2]))
+    coordinates = np.array(list(coordinate_iter))
+
+    multi_args = []
+    for coordinate in coordinates:
+        end_coordinate = coordinate + super_block_size
+        m = end_coordinate > dataset_bounds[3:]
+        end_coordinate[m] = dataset_bounds[3:][m]
+
+        multi_args.append([coordinate, end_coordinate, block_size,
+                           from_url, to_url, mip])
+
+    # Run multiprocessing
+    if n_threads == 1:
+        mu.multiprocess_func(_rewrite_image_thread, multi_args,
+                             n_threads=n_threads, verbose=True,
+                             debug=n_threads == 1)
+    else:
+        mu.multisubprocess_func(_rewrite_image_thread, multi_args,
                                 n_threads=n_threads)
