8172e68657446c36e8357503f56941d744dd8a75
sdorkenw <sven.dorkenwald@googlemail.com>
2018-07-18 11:19:42 -0700

    refurbished code from @nkem in branch nkem-meshing

---
+++ pychunkedgraph/meshing/meshgen.py
@@ -0,0 +1,133 @@
+import sys
+import os
+import numpy as np
+import itertools
+
+import cloudvolume
+from igneous.tasks import MeshTask
+
+sys.path.insert(0, os.path.join(sys.path[0], '../..'))
+os.environ['TRAVIS_BRANCH'] = "IDONTKNOWWHYINEEDTHIS"
+
+from pychunkedgraph.backend import chunkedgraph
+from pychunkedgraph.backend import multiprocessing_utils as mu
+
+
+def chunk_mesh_task(cg, chunk_id, ws_path, mip=3):
+    layer = cg.get_chunk_layer(chunk_id)
+
+    sv_to_node_mapping = {}
+    for seg_id in range(1, cg.get_max_node_id + 1):
+        node_id = cg.get_node_id(seg_id, chunk_id)
+
+        atomic_ids = cg.get_subgraph(node_id)
+
+        sv_to_node_mapping.update(dict(zip(atomic_ids,
+                                           [node_id] * len(atomic_ids))))
+
+    mesh_block_shape = cg.chunk_size // np.array([2**mip, 2**mip, 1])
+    chunk_offset = cg.get_chunk_coordinates(chunk_id) * \
+                   cg.fan_out ** np.max([0, (layer - 2)]) * cg.chunk_size
+
+    task = MeshTask(
+        mesh_block_shape,
+        chunk_offset,
+        ws_path,
+        mip=mip,
+        simplification_factor=400,
+        max_simplification_error=20,
+        remap_table=sv_to_node_mapping,
+        generate_manifests=True,
+        low_padding=1,
+        high_padding=1
+    )
+    task.execute()
+
+
+def _mesh_dataset_thread(args):
+    cg_info, start_block, end_block, ws_path, mip, layer = args
+
+    cg = chunkedgraph.ChunkedGraph(table_id=cg_info["table_id"],
+                                   instance_id=cg_info["instance_id"],
+                                   project_id=cg_info["project_id"],
+                                   credentials=cg_info["credentials"])
+
+    for block_z in range(start_block[2], end_block[2]):
+        z_start = block_z * cg.chunk_size[2]
+        z_end = (block_z + 1) * cg.chunk_size[2]
+        for block_y in range(start_block[1], end_block[1]):
+            y_start = block_y * cg.chunk_size[1]
+            y_end = (block_y + 1) * cg.chunk_size[1]
+            for block_x in range(start_block[0], end_block[0]):
+                x_start = block_x * cg.chunk_size[0]
+                x_end = (block_x + 1) * cg.chunk_size[0]
+
+                chunk_id = cg.get_chunk_id(x=x_start, y=y_start, z=z_start,
+                                           layer=layer)
+
+                chunk_mesh_task(cg=cg, chunk_id=chunk_id, ws_path=ws_path,
+                                mip=mip)
+
+
+def mesh_dataset(table_id, layer, mip=3, bounding_box=None, block_factor=1,
+                 n_threads=1):
+    if "pinky" in table_id:
+        ws_path = "gs://neuroglancer/svenmd/pinky40_v11/watershed/"
+    elif "basil" in table_id:
+        ws_path = "gs://neuroglancer/svenmd/basil_4k_oldnet_cg/watershed/"
+    else:
+        raise Exception("Dataset unknown")
+
+    cg = chunkedgraph.ChunkedGraph(table_id=table_id)
+
+    ws_cv = cloudvolume.CloudVolume(ws_path, mip=mip)
+
+    dataset_bounding_box = np.array(ws_cv.bounds.to_list())
+
+    block_bounding_box_cg = \
+        [np.floor(dataset_bounding_box[:3] / cg.chunk_size).astype(np.int),
+         np.ceil(dataset_bounding_box[3:] / cg.chunk_size).astype(np.int)]
+
+    if bounding_box is not None:
+        bounding_box_cg = \
+            [np.floor(bounding_box[0] / cg.chunk_size).astype(np.int),
+             np.ceil(bounding_box[1] / cg.chunk_size).astype(np.int)]
+
+        m = block_bounding_box_cg[0] < bounding_box_cg[0]
+        block_bounding_box_cg[0][m] = bounding_box_cg[0][m]
+
+        m = block_bounding_box_cg[1] > bounding_box_cg[1]
+        block_bounding_box_cg[1][m] = bounding_box_cg[1][m]
+
+    block_iter = itertools.product(np.arange(block_bounding_box_cg[0][0],
+                                             block_bounding_box_cg[1][0],
+                                             block_factor),
+                                   np.arange(block_bounding_box_cg[0][1],
+                                             block_bounding_box_cg[1][1],
+                                             block_factor),
+                                   np.arange(block_bounding_box_cg[0][2],
+                                             block_bounding_box_cg[1][2],
+                                             block_factor))
+
+    blocks = np.array(list(block_iter))
+    cg_info = cg.get_serialized_info()
+
+    multi_args = []
+    for start_block in blocks:
+        end_block = start_block + block_factor
+        m = end_block > block_bounding_box_cg[1]
+        end_block[m] = block_bounding_box_cg[1][m]
+
+        multi_args.append([cg_info, start_block, end_block, ws_path, mip,
+                           layer])
+
+    raise()
+
+    # Run multiprocessing
+    if n_threads == 1:
+        mu.multiprocess_func(_mesh_dataset_thread, multi_args,
+                             n_threads=n_threads, verbose=True,
+                             debug=n_threads == 1)
+    else:
+        mu.multisubprocess_func(_mesh_dataset_thread, multi_args,
+                                n_threads=n_threads)
