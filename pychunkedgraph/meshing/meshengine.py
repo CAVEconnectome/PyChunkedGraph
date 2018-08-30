166b0f7ca7720bccabc1c3ac50afc5647541e4ae
Sven Dorkenwald <sven.dorkenwald@googlemail.com>
2018-08-30 09:12:40 -0700

    added client side mesh merging capabilities

--- pychunkedgraph/meshing/meshengine.py
+++ pychunkedgraph/meshing/meshengine.py
@@ -4,7 +4,7 @@ import itertools
 import random
 
 from pychunkedgraph.backend import chunkedgraph
-from pychunkedgraph.multiprocessing import multiprocessing_utils as mu
+from pychunkedgraph.parallelizing import multiprocessing_utils as mu
 
 from . import meshgen
 
@@ -160,10 +160,9 @@ class MeshEngine(object):
 
             multi_args.append([cg_info, start_block, end_block, self.cg.cv_path,
                                self.cv_mesh_dir, self.mesh_mip, layer])
-
         random.shuffle(multi_args)
 
-        # Run multiprocessing
+        # Run parallelizing
         if n_threads == 1:
             mu.multiprocess_func(meshgen._mesh_layer_thread, multi_args,
                                  n_threads=n_threads, verbose=True,
@@ -190,7 +189,7 @@ class MeshEngine(object):
                                root_id_blocks[i_block + 1],
                                self.highest_mesh_layer])
 
-        # Run multiprocessing
+        # Run parallelizing
         if n_threads == 1:
             mu.multiprocess_func(meshgen._create_manifest_files_thread,
                                  multi_args, n_threads=n_threads, verbose=True,
