6945eb2f1e66f11327cb6ff8b64352706f9273c7
Sven Dorkenwald <sven.dorkenwald@googlemail.com>
2018-09-12 07:24:33 -0700

    corrected dependencies and setup.py

--- setup.py
+++ setup.py
@@ -3,27 +3,24 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('requirements.txt', 'r') as f:
+    required = f.read().splitlines()
+
 setup(
     name="PyChunkedGraph",
     version="0.1",
     author="Sven Dorkenwald",
-    author_email="",
+    author_email="svenmd@princeton.edu",
     description="Proofreading backend for Neuroglancer",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/seung-lab/PyChunkedGraph",
     packages=find_packages(),
-    install_requires=[
-        "google-cloud-bigtable",
-        "networkx",
-        "cloud-volume",
-        "bitstring",
-        "flask",
-        "flask_cors"
-    ],
+    install_requires=required,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX :: Linux",
     ],
+    dependency_links=['git+https://github.com/sdorkenw/MultiWrapper']
 )
