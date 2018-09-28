29672a3d68b024ed45f64a6781a112d3761f572e
Sven Dorkenwald <sven.dorkenwald@googlemail.com>
2018-09-28 18:00:54 -0400

    fix(uwsgi.ini) added nginx home to env

--- run.py
+++ run.py
@@ -1,19 +1,15 @@
-import sys
 from werkzeug.serving import WSGIRequestHandler
-import os
-
 from pychunkedgraph.app import create_app
 
 application = create_app()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    # Set HTTP protocol
     WSGIRequestHandler.protocol_version = "HTTP/1.1"
-    # WSGIRequestHandler.protocol_version = "HTTP/2.0"
 
     application.run(host='0.0.0.0',
                     port=4000,
                     debug=True,
-                    threaded=False,
+                    threaded=True,
                     ssl_context='adhoc')
+
