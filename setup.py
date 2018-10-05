8acafbc086edc416b70a7d9423b99bdbeffe801e
Forrest Collman <forrest.collman@gmail.com>
2018-10-05 08:13:04 -0700

    added CD infrastructure

--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
 from setuptools import setup, find_packages
+import re
+import os
+import codecs
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -8,7 +27,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name="PyChunkedGraph",
-    version="0.1",
+    version=find_version('pychunkedgraph', '__init__.py'),
     author="Sven Dorkenwald",
     author_email="svenmd@princeton.edu",
     description="Proofreading backend for Neuroglancer",
@@ -21,6 +40,5 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX :: Linux",
-    ],
-    dependency_links=['git+https://github.com/sdorkenw/MultiWrapper.git#egg=multiwrapper']
+    ]
 )
