e6c8f41724836447477c29f137a57c7141f5850b
Forrest Collman <forrest.collman@gmail.com>
2018-10-08 13:07:05 -0700

    adding versioning to blueprint

--- pychunkedgraph/app/cg_app_blueprint.py
+++ pychunkedgraph/app/cg_app_blueprint.py
@@ -11,7 +11,7 @@ import traceback
 
 from pychunkedgraph.app import app_utils
 
-
+__version__ = '0.1.6'
 bp = Blueprint('pychunkedgraph', __name__, url_prefix="/")
 
 # -------------------------------
@@ -21,7 +21,7 @@ bp = Blueprint('pychunkedgraph', __name__, url_prefix="/")
 @bp.route('/')
 @bp.route("/index")
 def index():
-    return "PyChunkedGraph Server -- 0.2"
+    return "PyChunkedGraph Server -- " + __version__
 
 
 @bp.route
