ea20a00c178c8b0b4cfe1a6885abb7dacbcf1cc5
Sven Dorkenwald <sven.dorkenwald@googlemail.com>
2018-10-08 13:35:48 -0700

    reset the url_prefix to segmentation

--- pychunkedgraph/app/cg_app_blueprint.py
+++ pychunkedgraph/app/cg_app_blueprint.py
@@ -12,7 +12,7 @@ import traceback
 from pychunkedgraph.app import app_utils
 
 __version__ = '0.1.7'
-bp = Blueprint('pychunkedgraph', __name__, url_prefix="/")
+bp = Blueprint('pychunkedgraph', __name__, url_prefix="/segmentation")
 
 # -------------------------------
 # ------ Access control and index
