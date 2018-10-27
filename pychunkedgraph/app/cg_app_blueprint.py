72be85b86fe0213845409b08a826aad845240a17
Sven Dorkenwald <sven.dorkenwald@googlemail.com>
2018-10-27 14:41:50 -0400

    Bump version: 0.1.53 → 0.1.54

--- pychunkedgraph/app/cg_app_blueprint.py
+++ pychunkedgraph/app/cg_app_blueprint.py
@@ -12,7 +12,7 @@ import collections
 
 from pychunkedgraph.app import app_utils
 
-__version__ = '0.1.53'
+__version__ = '0.1.54'
 bp = Blueprint('pychunkedgraph', __name__, url_prefix="/segmentation")
 
 # -------------------------------
