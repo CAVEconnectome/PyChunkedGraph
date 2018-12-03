111d8d29c149c9a06a0234b8fc03f8518db2489c
Nico Kemnitz <nkemnitz@princeton.edu>
2018-12-02 23:14:52 -0500

    performance(get_subgraph): Using row sets and removing numpy copies

--- pychunkedgraph/backend/chunkedgraph_utils.py
+++ pychunkedgraph/backend/chunkedgraph_utils.py
@@ -215,7 +215,7 @@ def partial_row_data_to_column_dict(partial_row_data: bigtable.row_data.PartialR
         -> Dict[column_keys._Column, bigtable.row_data.PartialRowData]:
     new_column_dict = {}
 
-    for family_id, column_dict in partial_row_data.cells.items():
+    for family_id, column_dict in partial_row_data._cells.items():
         for column_key, column_values in column_dict.items():
             column = column_keys.from_key(family_id, column_key)
             new_column_dict[column] = column_values
