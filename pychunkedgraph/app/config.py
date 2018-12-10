31b58b2c7b57c1f6de880f97a3c6b201275aac39
Forrest Collman <forrest.collman@gmail.com>
2018-12-10 08:36:35 -0800

    removing secret from source

--- pychunkedgraph/app/config.py
+++ pychunkedgraph/app/config.py
@@ -6,8 +6,7 @@ class BaseConfig(object):
     DEBUG = False
     TESTING = False
     HOME = os.path.expanduser("~")
-    # TODO get this secret out of source control
-    SECRET_KEY = '1d94e52c-1c89-4515-b87a-f48cf3cb7f0b'
+    SECRET_KEY = 'CONFIGURE_THIS_VIA_INSTANCE_CONFIG'
 
     LOGGING_FORMAT = '{"source":"%(name)s","time":"%(asctime)s","severity":"%(levelname)s","message":"%(message)s"}'
     LOGGING_DATEFORMAT = '%Y-%m-%dT%H:%M:%S.0Z'
