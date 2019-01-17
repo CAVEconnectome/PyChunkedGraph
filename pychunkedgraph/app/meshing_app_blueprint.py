f9ac989380b138b5f95baced7353681829b9a053
Sven Dorkenwald <sven.dorkenwald@googlemail.com>
2019-01-17 14:00:28 -0800

    Bump version: 0.1.94 → 0.1.95

--- pychunkedgraph/app/meshing_app_blueprint.py
+++ pychunkedgraph/app/meshing_app_blueprint.py
@@ -9,7 +9,7 @@ from pychunkedgraph.app import app_utils
 
 # os.environ['TRAVIS_BRANCH'] = "IDONTKNOWWHYINEEDTHIS"
 
-__version__ = '0.1.94'
+__version__ = '0.1.95'
 bp = Blueprint('pychunkedgraph_meshing', __name__, url_prefix="/meshing")
 
 # -------------------------------
