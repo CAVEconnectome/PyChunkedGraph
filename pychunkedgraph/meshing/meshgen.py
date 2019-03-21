Fixed stop_layer calculation

pychunkedgraph/meshing/meshgen.py
@@ -131,9 +131,10 @@ def get_l2_overlapping_remappings(cg, chunk_id, time_stamp=None):
                 neigh_parent_chunk_ids.append(parent_chunk_ids)
 
     # Find lowest common chunk
-    # stop_layer = np.where(np.unique(neigh_parent_chunk_ids, axis=1,
-    #                                 return_counts=True)[1] == 1)[0][0] + 3
-    stop_layer = cg.n_layers
+    neigh_parent_chunk_ids = np.array(neigh_parent_chunk_ids)
+    layer_agreement = np.all((neigh_parent_chunk_ids -
+                              neigh_parent_chunk_ids[0]) == 0, axis=0)
+    stop_layer = np.where(layer_agreement)[0][0] + 1
 
     # Find the parent in the lowest common chunk for each l2 id. These parent
     # ids are referred to as root ids even though they are not necessarily the