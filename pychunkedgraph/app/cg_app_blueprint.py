9b75b84e3640ea996305f6d254a0c6e46753b742
Sven Dorkenwald <sven.dorkenwald@googlemail.com>
2019-04-30 00:42:39 -0400

    Bump version: 0.1.110 → 0.1.111

--- pychunkedgraph/app/cg_app_blueprint.py
+++ pychunkedgraph/app/cg_app_blueprint.py
@@ -16,7 +16,7 @@ from pychunkedgraph.backend import chunkedgraph_exceptions as cg_exceptions
 from pychunkedgraph.meshing import meshgen
 
 
-__version__ = '0.1.110'
+__version__ = '0.1.111'
 bp = Blueprint('pychunkedgraph', __name__, url_prefix="/segmentation")
 
 # -------------------------------
