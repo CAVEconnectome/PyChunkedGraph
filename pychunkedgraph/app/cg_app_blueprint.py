df707ff8436e2d8b7164a05380b149847cde0c64
Sven Dorkenwald <sven.dorkenwald@googlemail.com>
2019-06-16 18:56:24 -0700

    added fafb version identifier

--- pychunkedgraph/app/cg_app_blueprint.py
+++ pychunkedgraph/app/cg_app_blueprint.py
@@ -16,7 +16,7 @@ from pychunkedgraph.backend import chunkedgraph_exceptions as cg_exceptions, \
     chunkedgraph_comp as cg_comp
 from middle_auth_client import auth_required, auth_requires_roles
 
-__version__ = '0.1.113'
+__version__ = 'fafb.1.0'
 bp = Blueprint('pychunkedgraph', __name__, url_prefix="/segmentation")
 
 # -------------------------------
