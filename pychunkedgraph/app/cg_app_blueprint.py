06296540bd278f1d782d24ee3f196a5b3013bfc5
Sven Dorkenwald <sven.dorkenwald@googlemail.com>
2019-08-06 13:48:59 -0700

    version bump

--- pychunkedgraph/app/cg_app_blueprint.py
+++ pychunkedgraph/app/cg_app_blueprint.py
@@ -17,7 +17,7 @@ from pychunkedgraph.backend import chunkedgraph_exceptions as cg_exceptions, \
 from pychunkedgraph.meshing import meshgen
 
 
-__version__ = 'pinky-prod.1.6'
+__version__ = 'pinky-prod.1.7'
 bp = Blueprint('pychunkedgraph', __name__, url_prefix="/segmentation")
 
 # -------------------------------
