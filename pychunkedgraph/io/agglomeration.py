9d99bdfc38f45e39cf4136c3a0c8545683b7d3a3
Akhilesh Halageri <akhileshhalageri@gmail.com>
2019-09-12 19:08:59 +0000

    feat: use protobuf for mapping update file name

--- pychunkedgraph/io/agglomeration.py
+++ pychunkedgraph/io/agglomeration.py
@@ -24,9 +24,9 @@ def deserialize(mapping_message: ChunkMappingMsg) -> Dict:
 
 
 def put_chunk_agglomeration(agglomeration_dir, mapping, chunk_coord) -> None:
-    # filename format - chunk_x_y_z.serliazation
+    # filename format - mapping_x_y_z.serliazation
     mapping_message = serialize(mapping)
-    file_name = f"chunk_{'_'.join(str(coord) for coord in chunk_coord)}.json"
+    file_name = f"mapping_{'_'.join(str(coord) for coord in chunk_coord)}.proto"
     with SimpleStorage(agglomeration_dir) as storage:
         storage.put_file(
             file_path=file_name,
@@ -37,7 +37,8 @@ def put_chunk_agglomeration(agglomeration_dir, mapping, chunk_coord) -> None:
 
 
 def get_chunk_agglomeration(agglomeration_dir, chunk_coord):
-    file_name = f"chunk_{'_'.join(str(coord) for coord in chunk_coord)}.json"
+    # filename format - mapping_x_y_z.serliazation
+    file_name = f"mapping_{'_'.join(str(coord) for coord in chunk_coord)}.proto"
     with SimpleStorage(agglomeration_dir) as storage:
         mapping_message = ChunkMappingMsg()
         mapping_message.ParseFromString(storage.get_file(file_name))
