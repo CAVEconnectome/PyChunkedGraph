86633c79b80627f80b0733ed53762622de1f1d57
Nico Kemnitz <nkemnitz@princeton.edu>
2019-09-23 21:33:49 -0400

    refactor(app): jsonify remaining v1 endpoints

--- pychunkedgraph/app/segmentation/legacy/routes.py
+++ pychunkedgraph/app/segmentation/legacy/routes.py
@@ -1,6 +1,10 @@
-from flask import Blueprint
-from middle_auth_client import auth_requires_permission
+import json
 
+import numpy as np
+
+from flask import Blueprint, jsonify, request
+from middle_auth_client import auth_requires_permission
+from pychunkedgraph.app import app_utils
 from pychunkedgraph.app.segmentation import common
 from pychunkedgraph.backend import chunkedgraph_exceptions as cg_exceptions
 
@@ -70,13 +74,16 @@ def handle_info(table_id):
 @bp.route("/<table_id>/graph/root", methods=["POST", "GET"])
 @auth_requires_permission("view")
 def handle_root_1(table_id):
-    return common.handle_root_1(table_id)
+    atomic_id = np.uint64(json.loads(request.data)[0])
+    root_id = common.handle_root(table_id, atomic_id)
+    return app_utils.tobinary(root_id)
 
 
 @bp.route("/<table_id>/graph/<atomic_id>/root", methods=["POST", "GET"])
 @auth_requires_permission("view")
 def handle_root_2(table_id, atomic_id):
-    return common.handle_root_2(table_id, atomic_id)
+    root_id = common.handle_root(table_id, atomic_id)
+    return app_utils.tobinary(root_id)
 
 
 ### MERGE ----------------------------------------------------------------------
@@ -85,7 +92,8 @@ def handle_root_2(table_id, atomic_id):
 @bp.route("/<table_id>/graph/merge", methods=["POST", "GET"])
 @auth_requires_permission("edit")
 def handle_merge(table_id):
-    return common.handle_merge(table_id, bin_return=True)
+    merge_result = common.handle_merge(table_id)
+    return app_utils.tobinary(merge_result.new_root_ids)
 
 
 ### SPLIT ----------------------------------------------------------------------
@@ -94,7 +102,8 @@ def handle_merge(table_id):
 @bp.route("/<table_id>/graph/split", methods=["POST", "GET"])
 @auth_requires_permission("edit")
 def handle_split(table_id):
-    return common.handle_split(table_id, bin_return=True)
+    split_result = common.handle_split(table_id)
+    return app_utils.tobinary(split_result.new_root_ids)
 
 
 ### CHILDREN -------------------------------------------------------------------
@@ -103,7 +112,8 @@ def handle_split(table_id):
 @bp.route("/<table_id>/segment/<parent_id>/children", methods=["POST", "GET"])
 @auth_requires_permission("view")
 def handle_children(table_id, parent_id):
-    return common.handle_children(table_id, parent_id)
+    children_ids = common.handle_children(table_id, parent_id)
+    return app_utils.tobinary(children_ids)
 
 
 ### LEAVES ---------------------------------------------------------------------
@@ -112,7 +122,8 @@ def handle_children(table_id, parent_id):
 @bp.route("/<table_id>/segment/<root_id>/leaves", methods=["POST", "GET"])
 @auth_requires_permission("view")
 def handle_leaves(table_id, root_id):
-    return common.handle_leaves(table_id, root_id)
+    leaf_ids = common.handle_leaves(table_id, root_id)
+    return app_utils.tobinary(leaf_ids)
 
 
 ### LEAVES FROM LEAVES ---------------------------------------------------------
@@ -121,7 +132,8 @@ def handle_leaves(table_id, root_id):
 @bp.route("/<table_id>/segment/<atomic_id>/leaves_from_leave", methods=["POST", "GET"])
 @auth_requires_permission("view")
 def handle_leaves_from_leave(table_id, atomic_id):
-    return common.handle_leaves_from_leave(table_id, atomic_id)
+    leaf_ids = common.handle_leaves_from_leave(table_id, atomic_id)
+    return app_utils.tobinary(leaf_ids)
 
 
 ### SUBGRAPH -------------------------------------------------------------------
@@ -130,7 +142,8 @@ def handle_leaves_from_leave(table_id, atomic_id):
 @bp.route("/<table_id>/segment/<root_id>/subgraph", methods=["POST", "GET"])
 @auth_requires_permission("view")
 def handle_subgraph(table_id, root_id):
-    return common.handle_subgraph(table_id, root_id)
+    subgraph_result = common.handle_subgraph(table_id, root_id)
+    return app_utils.tobinary(subgraph_result)
 
 
 ### CHANGE LOG -----------------------------------------------------------------
@@ -139,19 +152,23 @@ def handle_subgraph(table_id, root_id):
 @bp.route("/<table_id>/segment/<root_id>/change_log", methods=["POST", "GET"])
 @auth_requires_permission("view")
 def change_log(table_id, root_id):
-    return common.change_log(table_id, root_id)
+    log = common.change_log(table_id, root_id)
+    return jsonify(log)
 
 
 @bp.route("/<table_id>/segment/<root_id>/merge_log", methods=["POST", "GET"])
 @auth_requires_permission("view")
 def merge_log(table_id, root_id):
-    return common.merge_log(table_id, root_id)
+    log = common.merge_log(table_id, root_id)
+    return jsonify(log)
 
 
 @bp.route("/<table_id>/graph/oldest_timestamp", methods=["POST", "GET"])
 @auth_requires_permission("view")
 def oldest_timestamp(table_id):
-    return common.oldest_timestamp(table_id)
+    earliest_timestamp = common.oldest_timestamp(table_id)
+    resp = {"iso": str(earliest_timestamp)}
+    return jsonify(resp)
 
 
 ### CONTACT SITES --------------------------------------------------------------
@@ -160,4 +177,5 @@ def oldest_timestamp(table_id):
 @bp.route("/<table_id>/segment/<root_id>/contact_sites", methods=["POST", "GET"])
 @auth_requires_permission("view")
 def handle_contact_sites(table_id, root_id):
-    return common.handle_contact_sites(table_id, root_id)
+    contact_sites = common.handle_contact_sites(table_id, root_id)
+    return jsonify(contact_sites)
