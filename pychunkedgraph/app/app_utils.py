5cd635dbdb2d9d7b09aff495ce61daf170f3a6c3
Nico Kemnitz <nkemnitz@princeton.edu>
2019-09-26 11:25:27 -0400

    chore(app): app_utils cleanup

--- pychunkedgraph/app/app_utils.py
+++ pychunkedgraph/app/app_utils.py
@@ -1,55 +1,52 @@
+import logging
+import sys
+import time
+
+import numpy as np
 from flask import current_app, json
-from google.auth import credentials, default as default_creds
+from google.auth import credentials
+from google.auth import default as default_creds
 from google.cloud import bigtable, datastore
 
-import sys
-import numpy as np
-import logging
-import time
-import redis
-import functools
-
-from pychunkedgraph.logging import jsonformatter, flask_log_db
 from pychunkedgraph.backend import chunkedgraph
+from pychunkedgraph.logging import flask_log_db, jsonformatter
 
-cache = {}
+CACHE = {}
 
 
 class DoNothingCreds(credentials.Credentials):
     def refresh(self, request):
         pass
 
+
 def jsonify_with_kwargs(data, **kwargs):
-    kwargs.setdefault('separators', (",", ":"))
+    kwargs.setdefault("separators", (",", ":"))
 
     if current_app.config["JSONIFY_PRETTYPRINT_REGULAR"] or current_app.debug:
-        kwargs['indent'] = 2
-        kwargs['separators'] = (", ", ": ")
+        kwargs["indent"] = 2
+        kwargs["separators"] = (", ", ": ")
 
     return current_app.response_class(
-        json.dumps(data, **kwargs) + "\n",
-        mimetype=current_app.config["JSONIFY_MIMETYPE"],
+        json.dumps(data, **kwargs) + "\n", mimetype=current_app.config["JSONIFY_MIMETYPE"]
     )
 
 
 def get_bigtable_client(config):
-    project_id = config.get('project_id', 'pychunkedgraph')
+    project_id = config.get("project_id", "pychunkedgraph")
 
-    if config.get('emulate', False):
+    if config.get("emulate", False):
         credentials = DoNothingCreds()
     else:
         credentials, project_id = default_creds()
 
-    client = bigtable.Client(admin=True,
-                             project=project_id,
-                             credentials=credentials)
+    client = bigtable.Client(admin=True, project=project_id, credentials=credentials)
     return client
 
 
 def get_datastore_client(config):
-    project_id = config.get('project_id', 'pychunkedgraph')
+    project_id = config.get("project_id", "pychunkedgraph")
 
-    if config.get('emulate', False):
+    if config.get("emulate", False):
         credentials = DoNothingCreds()
     else:
         credentials, project_id = default_creds()
@@ -59,46 +56,49 @@ def get_datastore_client(config):
 
 
 def get_cg(table_id):
-    assert table_id.startswith("fly") or table_id.startswith("golden") or \
-           table_id.startswith("pinky100_rv")
+    assert (
+        table_id.startswith("fly")
+        or table_id.startswith("golden")
+        or table_id.startswith("pinky100_rv")
+    )
 
-    if table_id not in cache:
-        instance_id = current_app.config['CHUNKGRAPH_INSTANCE_ID']
+    if table_id not in CACHE:
+        instance_id = current_app.config["CHUNKGRAPH_INSTANCE_ID"]
         client = get_bigtable_client(current_app.config)
 
         # Create ChunkedGraph logging
         logger = logging.getLogger(f"{instance_id}/{table_id}")
-        logger.setLevel(current_app.config['LOGGING_LEVEL'])
+        logger.setLevel(current_app.config["LOGGING_LEVEL"])
 
         # prevent duplicate logs from Flasks(?) parent logger
         logger.propagate = False
 
         handler = logging.StreamHandler(sys.stdout)
-        handler.setLevel(current_app.config['LOGGING_LEVEL'])
+        handler.setLevel(current_app.config["LOGGING_LEVEL"])
         formatter = jsonformatter.JsonFormatter(
-            fmt=current_app.config['LOGGING_FORMAT'],
-            datefmt=current_app.config['LOGGING_DATEFORMAT'])
+            fmt=current_app.config["LOGGING_FORMAT"],
+            datefmt=current_app.config["LOGGING_DATEFORMAT"],
+        )
         formatter.converter = time.gmtime
         handler.setFormatter(formatter)
 
         logger.addHandler(handler)
 
         # Create ChunkedGraph
-        cache[table_id] = chunkedgraph.ChunkedGraph(table_id=table_id,
-                                                    instance_id=instance_id,
-                                                    client=client,
-                                                    logger=logger)
+        CACHE[table_id] = chunkedgraph.ChunkedGraph(
+            table_id=table_id, instance_id=instance_id, client=client, logger=logger
+        )
+
     current_app.table_id = table_id
-    return cache[table_id]
+    return CACHE[table_id]
 
 
 def get_log_db(table_id):
-    if 'log_db' not in cache:
+    if "log_db" not in CACHE:
         client = get_datastore_client(current_app.config)
-        cache["log_db"] = flask_log_db.FlaskLogDatabase(table_id,
-                                                        client=client)
+        CACHE["log_db"] = flask_log_db.FlaskLogDatabase(table_id, client=client)
 
-    return cache["log_db"]
+    return CACHE["log_db"]
 
 
 def toboolean(value):
