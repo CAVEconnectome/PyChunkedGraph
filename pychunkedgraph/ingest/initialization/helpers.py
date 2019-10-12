b7f1c8bc0ee6a9f06e61355e74a954e2b5879aca
Akhilesh Halageri <akhileshhalageri@gmail.com>
2019-10-12 19:18:51 +0000

    get relevant atomic chunk coords

--- pychunkedgraph/ingest/initialization/helpers.py
+++ pychunkedgraph/ingest/initialization/helpers.py
@@ -10,11 +10,40 @@ def get_touching_atomic_chunks(
     chunkedgraph_meta: ChunkedGraphMeta, layer: int, chunk_coords: Sequence[int]
 ):
     """get atomic chunks along touching faces of children chunks of a parent chunk"""
+    chunk_coords = np.array(chunk_coords, dtype=int)
+    touching_atomic_chunks = set()
 
-    parent_atomic_chunk_count = chunkedgraph_meta.graph_config.fanout ** (layer - 2)
+    atomic_chunk_count = chunkedgraph_meta.graph_config.fanout ** (layer - 2)
+    layer2_chunk_bounds = chunkedgraph_meta.layer_chunk_bounds[2]
 
-    layer2_chunk_boundaries = chunkedgraph_meta.layer_chunk_bounds[2]
-    children_layer = layer - 1
+    chunk_offset = chunk_coords * atomic_chunk_count
+    mid = (atomic_chunk_count // 2) - 1
 
+    # relevant chunks along touching planes at center
+    for axis_1 in range(atomic_chunk_count):
+        for axis_2 in range(atomic_chunk_count):
+            # x-y plane
+            chunk_1 = chunk_offset + np.array((axis_1, axis_2, mid))
+            chunk_2 = chunk_offset + np.array((axis_1, axis_2, mid + 1))
+            touching_atomic_chunks.add(chunk_1)
+            touching_atomic_chunks.add(chunk_2)
 
+            # x-z plane
+            chunk_1 = chunk_offset + np.array((axis_1, mid, axis_2))
+            chunk_2 = chunk_offset + np.array((axis_1, mid + 1, axis_2))
+            touching_atomic_chunks.add(chunk_1)
+            touching_atomic_chunks.add(chunk_2)
+
+            # y-z plane
+            chunk_1 = chunk_offset + np.array((mid, axis_1, axis_2))
+            chunk_2 = chunk_offset + np.array((mid + 1, axis_1, axis_2))
+            touching_atomic_chunks.add(chunk_1)
+            touching_atomic_chunks.add(chunk_2)
+
+    result = []
+    for coords in touching_atomic_chunks:
+        if np.all(np.less(coords, layer2_chunk_bounds)):
+            result.append(coords)
+
+    return np.array(result, dtype=int)
 
