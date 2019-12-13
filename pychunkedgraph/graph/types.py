3f1399f6e501d25c9f73d01ebc9d50717f69cfb3
Akhilesh Halageri <akhileshhalageri@gmail.com>
2019-12-13 20:24:43 +0000

    docs

--- pychunkedgraph/graph/types.py
+++ pychunkedgraph/graph/types.py
@@ -4,6 +4,8 @@ import numpy as np
 
 from .utils import basetypes
 
+# An Agglomeration is syntactic sugar for representing
+# a level 2 ID and it's supervoxels and edges.
 _agglomeration_fields = ("supervoxels", "edges")
 _agglomeration_defaults = (
     np.array([], dtype=basetypes.NODE_ID),
