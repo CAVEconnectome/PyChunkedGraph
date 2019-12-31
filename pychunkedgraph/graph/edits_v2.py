beb77df48d700cdeb5eea03caabaa4de7fc596ff
Akhilesh Halageri <akhileshhalageri@gmail.com>
2019-12-31 17:14:20 +0000

    wip: merge

--- pychunkedgraph/graph/edits_v2.py
+++ pychunkedgraph/graph/edits_v2.py
@@ -36,6 +36,17 @@ def _get_all_siblings(cg, new_id_ce_siblings: Iterable) -> List:
     return cg.get_children(np.unique(cg.get_parents(new_id_ce_siblings)), flatten=True)
 
 
+def _handle_skip_hierarchy(cg, new_node: Node, jump_to_layer: int) -> Node:
+    new_id = new_node.node_id
+    parent_chunk_id = cg.get_parent_chunk_id_dict(new_id)[jump_to_layer]
+    new_parent_seg_id = cg.id_client.create_segment_id(parent_chunk_id)
+    new_parent_id = parent_chunk_id | new_parent_seg_id
+    new_parent_node = Node(new_parent_id)
+    new_node.parent_id = new_parent_id
+    new_parent_node.children = [new_id]
+    return new_parent_node
+
+
 def _create_parents(
     cg,
     new_cross_edges_d_d: Dict[np.uint64, Dict],
@@ -52,21 +63,16 @@ def _create_parents(
             continue
         new_ids = layer_new_ids_d[current_layer]
         for new_id in new_ids:
-            node = Node(new_id)
-            all_new_ids[new_id] = node
+            new_node = Node(new_id)
+            all_new_ids[new_id] = new_node
             if not new_id in new_cross_edges_d_d:
                 new_cross_edges_d_d[new_id] = cg.get_cross_chunk_edges(new_id)
             new_id_ce_d = new_cross_edges_d_d[new_id]
             new_id_ce_layer = list(new_id_ce_d.keys())[0]
             if not new_id_ce_layer == current_layer:
-                # create new id at that level
-                parent_chunk_id = cg.get_parent_chunk_id_dict(new_id)[new_id_ce_layer]
-                new_parent_seg_id = cg.id_client.create_segment_id(parent_chunk_id)
-                new_parent_id = parent_chunk_id | new_parent_seg_id
-                new_parent_node = Node(new_parent_id)
-                new_parent_node.children = [new_id]
-                all_new_ids[new_parent_id] = new_parent_node
-                layer_new_ids_d[new_id_ce_layer].append(new_parent_id)
+                new_parent_node = _handle_skip_hierarchy(cg, new_node, new_id_ce_layer)
+                all_new_ids[new_parent_node.node_id] = new_parent_node
+                layer_new_ids_d[new_id_ce_layer].append(new_parent_node.node_id)
             else:
                 new_id_ce_siblings = new_id_ce_d[new_id_ce_layer][:, 1]
                 new_id_all_siblings = _get_all_siblings(cg, new_id_ce_siblings)
