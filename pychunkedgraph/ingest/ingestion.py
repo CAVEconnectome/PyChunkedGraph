aaa62b1d68a14fb9ab9265cb2716105b97c3dbda
Akhilesh Halageri <akhileshhalageri@gmail.com>
2020-01-14 19:43:27 +0000

    wip: worker per chunk model for handling failed jobs

--- pychunkedgraph/ingest/ingestion.py
+++ pychunkedgraph/ingest/ingestion.py
@@ -1,17 +1,9 @@
-"""
-Ingest / create chunkedgraph on a single machine / instance
-"""
-
 import time
-import multiprocessing as mp
-from itertools import product
-from typing import List
-from typing import Sequence
 from typing import Dict
 from typing import Tuple
+from multiprocessing import RLock
 
 import numpy as np
-from multiwrapper import multiprocessing_utils as mu
 
 from .types import ChunkTask
 from .manager import IngestionManager
@@ -28,54 +20,24 @@ from ..backend.chunks.hierarchy import get_children_coords
 chunk_id_str = lambda layer, coords: f"{layer}_{'_'.join(map(str, coords))}"
 
 
-def start_ingest(imanager: IngestionManager):
-    atomic_chunk_bounds = imanager.cg_meta.layer_chunk_bounds[2]
-    chunk_coords = list(product(*[range(r) for r in atomic_chunk_bounds]))
-    np.random.shuffle(chunk_coords)
-
-    with mp.Manager() as manager:
-        parent_children_count_d_shared = manager.dict()
-        parent_children_count_d_lock = manager.RLock()  # pylint: disable=no-member
-        jobs = chunked(chunk_coords, len(chunk_coords) // mp.cpu_count())
-        multi_args = []
-        for job in jobs:
-            multi_args.append(
-                (
-                    parent_children_count_d_shared,
-                    parent_children_count_d_lock,
-                    imanager.get_serialized_info(),
-                    job,
-                )
-            )
-        mu.multiprocess_func(
-            _create_atomic_chunks_helper,
-            multi_args,
-            n_threads=min(len(multi_args), mp.cpu_count()),
-        )
-
-
 def get_parent_task(
     parent_children_count_d_shared: Dict,
-    parent_children_count_d_lock: mp.Lock,
+    parent_children_count_d_lock: RLock,
     imanager: IngestionManager,
     layer: int,
     coords: np.ndarray,
 ):
     parent_layer = layer + 1
+    parent_coords = np.array(coords, int) // imanager.cg_meta.graph_config.fanout
     if parent_layer > imanager.cg_meta.layer_count:
-        return
+        return ChunkTask(imanager.cg_meta, parent_coords, parent_layer)
 
-    parent_coords = (
-        np.array(coords, int) // imanager.cg_meta.graph_config.fanout
-    )
     parent_chunk_str = chunk_id_str(parent_layer, parent_coords)
 
     with parent_children_count_d_lock:
         if not parent_chunk_str in parent_children_count_d_shared:
             children_count = len(
-                get_children_coords(
-                    imanager.cg_meta, parent_layer, parent_coords
-                )
+                get_children_coords(imanager.cg_meta, parent_layer, parent_coords)
             )
             # set initial number of child chunks
             parent_children_count_d_shared[parent_chunk_str] = children_count
@@ -86,29 +48,28 @@ def get_parent_task(
         # if zero, all dependents complete -> start parent
         if parent_children_count_d_shared[parent_chunk_str] == 0:
             parent_children_count_d_shared.pop(parent_chunk_str, None)
-            return ChunkTask(imanager.cg_meta)
+            return ChunkTask(imanager.cg_meta, parent_coords, parent_layer)
 
 
-def create_atomic_chunk_helper(args):
+def create_atomic_chunk_helper(
+    parent_children_count_d_shared: Dict,
+    parent_children_count_d_lock: RLock,
+    im_info: dict,
+    coords: np.ndarray,
+):
     """Helper to queue atomic chunk task."""
-    (
-        parent_children_count_d_shared,
-        parent_children_count_d_lock,
-        im_info,
-        chunk_coords,
-    ) = args
     imanager = IngestionManager(**im_info)
-    chunk_coords = np.array(list(chunk_coords), dtype=np.int)
-    chunk_edges_all, mapping = _get_atomic_chunk_data(imanager, chunk_coords)
+    coords = np.array(list(coords), dtype=np.int)
+    chunk_edges_all, mapping = _get_atomic_chunk_data(imanager, coords)
 
     ids, affs, areas, isolated = get_chunk_data_old_format(chunk_edges_all, mapping)
     imanager.cg.add_atomic_edges_in_chunks(ids, affs, areas, isolated)
-    get_parent_task(
+    return get_parent_task(
         parent_children_count_d_shared,
         parent_children_count_d_lock,
         imanager,
         2,
-        chunk_coords,
+        coords,
     )
 
 
@@ -126,7 +87,7 @@ def create_parent_chunk_helper(args):
 
     children = get_children_coords(imanager.cg_meta, layer, chunk_coords)
     imanager.cg.add_layer(layer, children)
-    get_parent_task(
+    return get_parent_task(
         parent_children_count_d_shared,
         parent_children_count_d_lock,
         imanager,
@@ -148,9 +109,7 @@ def _get_atomic_chunk_data(imanager: IngestionManager, coord) -> Tuple[Dict, Dic
     mapping = (
         read_raw_agglomeration_data(imanager, coord)
         if imanager.cg_meta.data_source.use_raw_components
-        else get_chunk_components(
-            imanager.cg_meta.data_source.components, coord
-        )
+        else get_chunk_components(imanager.cg_meta.data_source.components, coord)
     )
     return chunk_edges, mapping
 
--- pychunkedgraph/ingest/main.py
+++ pychunkedgraph/ingest/main.py
@@ -1,177 +1,63 @@
+"""
+Ingest / create chunkedgraph on a single machine / instance
+"""
+
 import time
 import random
+from itertools import product
+from multiprocessing import Queue
+from multiprocessing import Process
+from multiprocessing import Manager
+from multiprocessing import cpu_count
+from multiprocessing import current_process
 
-from multiprocessing import Process, Queue, current_process
+from numpy.random import shuffle
 
-#
-# Function run by worker processes
-#
-
-
-def worker(input, output):
-    for func, args in iter(input.get, "STOP"):
-        result = calculate(func, args)
-        output.put(result)
-
-
-#
-# Function used to calculate result
-#
+from .types import ChunkTask
+from .manager import IngestionManager
+from .ingestion import create_atomic_chunk_helper
+from .ingestion import create_parent_chunk_helper
 
 
-def calculate(func, args):
-    result = func(*args)
-    return "%s says that %s%s = %s" % (
-        current_process().name,
-        func.__name__,
-        args,
-        result,
-    )
-
-
-#
-# Functions referenced by tasks
-#
-
-
-def mul(a, b):
-    time.sleep(0.5 * random.random())
-    return a * b
-
-
-def plus(a, b):
-    time.sleep(0.5 * random.random())
-    return a + b
-
-
-#
-#
-#
-
-
-def test():
-    NUMBER_OF_PROCESSES = 4
-    TASKS1 = [(mul, (i, 7)) for i in range(20)]
-    TASKS2 = [(plus, (i, 8)) for i in range(10)]
-
-    # Create queues
-    task_queue = Queue()
-    done_queue = Queue()
-
-    # Submit tasks
-    for task in TASKS1:
-        task_queue.put(task)
-
-    # Start worker processes
-    for i in range(NUMBER_OF_PROCESSES):
-        Process(target=worker, args=(task_queue, done_queue)).start()
-
-    # Get and print results
-    print("Unordered results:")
-    for i in range(len(TASKS1)):
-        print("\t", done_queue.get())
-
-    # Add more tasks using `put()`
-    for task in TASKS2:
-        task_queue.put(task)
-
-    # Get and print some more results
-    for i in range(len(TASKS2)):
-        print("\t", done_queue.get())
-
-    # Tell child processes to stop
-    for i in range(NUMBER_OF_PROCESSES):
-        task_queue.put("STOP")
-
-
-def _post_task_completion(
-    parent_children_count_d_shared: Dict,
-    parent_children_count_d_lock: mp.Lock,
-    imanager: IngestionManager,
-    layer: int,
-    coords: np.ndarray,
-):
-    parent_layer = layer + 1
-    if parent_layer > imanager.cg_meta.layer_count:
-        return
-
-    parent_coords = (
-        np.array(coords, int) // imanager.cg_meta.graph_config.fanout
-    )
-    parent_chunk_str = chunk_id_str(parent_layer, parent_coords)
-
-    with parent_children_count_d_lock:
-        if not parent_chunk_str in parent_children_count_d_shared:
-            children_count = len(
-                get_children_coords(
-                    imanager.cg_meta, parent_layer, parent_coords
-                )
-            )
-            # set initial number of child chunks
-            parent_children_count_d_shared[parent_chunk_str] = children_count
-
-        # decrement child count by 1
-        parent_children_count_d_shared[parent_chunk_str] -= 1
-
-        # if zero, all dependents complete -> start parent
-        if parent_children_count_d_shared[parent_chunk_str] == 0:
-            parent_children_count_d_shared.pop(parent_chunk_str, None)
-            children = get_children_coords(
-                imanager.cg_meta, parent_layer, parent_coords
-            )
-            imanager.cg.add_layer(parent_layer, children)
-            _post_task_completion(
-                parent_children_count_d_shared,
-                parent_children_count_d_lock,
-                imanager,
-                parent_layer,
-                parent_coords,
-            )
-
-
-def _create_atomic_chunks_helper(args):
-    """ helper to start atomic tasks """
-    (
-        parent_children_count_d_shared,
-        parent_children_count_d_lock,
-        im_info,
-        chunk_coords,
-    ) = args
-    imanager = IngestionManager(**im_info)
-    chunk_coords = np.array(list(chunk_coords), dtype=np.int)
-    chunk_edges_all, mapping = _get_atomic_chunk_data(imanager, chunk_coords)
-
-    ids, affs, areas, isolated = get_chunk_data_old_format(chunk_edges_all, mapping)
-    imanager.cg.add_atomic_edges_in_chunks(ids, affs, areas, isolated)
-    _post_task_completion(
-        parent_children_count_d_shared,
-        parent_children_count_d_lock,
-        imanager,
-        2,
-        chunk_coords,
-    )
-
-
-def start_ingest(imanager: IngestionManager):
-    atomic_chunk_bounds = imanager.cg_meta.layer_chunk_bounds[2]
-    chunk_coords = list(product(*[range(r) for r in atomic_chunk_bounds]))
-    np.random.shuffle(chunk_coords)
-
-    with mp.Manager() as manager:
-        parent_children_count_d_shared = manager.dict()
-        parent_children_count_d_lock = manager.RLock()  # pylint: disable=no-member
-        multi_args = []
-        for chunk_coords in chunks_coords:
-            multi_args.append(
+def worker(task_queue):
+    for func, args in iter(task_queue.get, "STOP"):
+        parent_task = func(*args)
+        task_queue.put(
+            (
+                create_atomic_chunk_helper,
                 (
                     parent_children_count_d_shared,
                     parent_children_count_d_lock,
                     imanager.get_serialized_info(),
-                    chunk_coords,
+                    coords,
+                ),
+            )
+        )
+
+
+def start_ingest(imanager: IngestionManager):
+    NUMBER_OF_PROCESSES = cpu_count()
+    atomic_chunk_bounds = imanager.cg_meta.layer_chunk_bounds[2]
+    chunks_coords = list(product(*[range(r) for r in atomic_chunk_bounds]))
+    shuffle(chunks_coords)
+
+    task_queue = Queue()
+    with Manager() as manager:
+        parent_children_count_d_shared = manager.dict()
+        parent_children_count_d_lock = manager.RLock()  # pylint: disable=no-member
+
+        for coords in chunks_coords:
+            task_queue.put(
+                (
+                    create_atomic_chunk_helper,
+                    (
+                        parent_children_count_d_shared,
+                        parent_children_count_d_lock,
+                        imanager.get_serialized_info(),
+                        coords,
+                    ),
                 )
             )
-        mu.multiprocess_func(
-            _create_atomic_chunks_helper,
-            multi_args,
-            n_threads=min(len(multi_args), mp.cpu_count()),
-        )
+
+        for _ in range(NUMBER_OF_PROCESSES):
+            Process(target=worker, args=(task_queue)).start()
