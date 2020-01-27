85d0e37a157e1172d80ff71d3c68f2ae05fe61ba
Sven Dorkenwald <sven.dorkenwald@googlemail.com>
2020-01-27 09:48:28 -0800

    fixed request type naming

--- pychunkedgraph/app/meshing/common.py
+++ pychunkedgraph/app/meshing/common.py
@@ -146,7 +146,7 @@ def api_exception(e):
 
 
 def handle_valid_frags(table_id, node_id):
-    current_app.request_type = "root"
+    current_app.request_type = "fragments"
     current_app.table_id = table_id
 
     user_id = str(g.auth_user["id"])
@@ -170,7 +170,7 @@ def handle_get_manifest(table_id, node_id):
     else:
         data = {}
 
-    current_app.request_type = "root"
+    current_app.request_type = "manifest"
     current_app.table_id = table_id
 
     user_id = str(g.auth_user["id"])
