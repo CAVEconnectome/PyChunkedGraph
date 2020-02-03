b97e53165f71c312fa42374df07d4598f04b4c90
Akhilesh Halageri <akhileshhalageri@gmail.com>
2020-02-03 19:50:07 +0000

    indent print within context

--- pychunkedgraph/graph/utils/context_managers.py
+++ pychunkedgraph/graph/utils/context_managers.py
@@ -1,13 +1,29 @@
+import builtins
+import traceback
 from time import time
+
+indent = 2
+
+builtin_print = builtins.print
+
+
+def foo(*args, **kwargs):
+    builtin_print(indent * " ", *args, **kwargs)
 
 
 class TimeIt:
-    def __init__(self, message=""):
+    def __init__(self, message="", tablen=2):
         self._message = message
         self._start = None
 
+        global indent
+        indent = tablen
+
     def __enter__(self):
+        print(f"Start {self._message}")
         self._start = time()
+        builtins.print = foo
 
     def __exit__(self, *args):
-        print(f"{self._message} {time()-self._start}")
+        print(f"{time()-self._start}")
+        builtins.print = builtin_print
