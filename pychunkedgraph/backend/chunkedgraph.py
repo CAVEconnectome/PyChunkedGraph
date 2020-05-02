2eac404bf0e5235451789c8e231017c6f940b69c
Akhilesh Halageri <akhileshhalageri@gmail.com>
2020-05-02 17:19:08 +0000

    test run sharded manifest

--- pychunkedgraph/backend/chunkedgraph.py
+++ pychunkedgraph/backend/chunkedgraph.py
@@ -3914,7 +3914,7 @@ class ChunkedGraph(object):
 
         if not children:
             return []
-        return [x[0].timestamp for x in children.values()]
+        return np.array([x[0].timestamp for x in children.values()], dtype=np.datetime64)
         # return {x: children[x][0].value
         #             if x in children else np.empty(0, dtype=basetypes.NODE_ID)
         #         for x in node_id}
