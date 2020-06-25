fix: speed up chunk ID calculation for array of coords

pychunkedgraph/graph/chunkedgraph.py
@@ -746,42 +746,54 @@ class ChunkedGraph:
         `parent_ids` must contain node IDs at same layer.
         TODO what have i done (describe algo)
         """
-        parents_layer = self.get_chunk_layer(parent_ids[0])
-        parent_coords_d = {
-            node_id: self.get_chunk_coordinates(node_id) for node_id in parent_ids
-        }
-
         from collections import defaultdict
+        from .utils.context_managers import TimeIt
 
-        parent_bounding_chunk_ids = defaultdict(lambda: types.empty_1d)
-        parent_layer_mask = {}
+        with TimeIt("_get_bounding_l2_children init"):
+            parents_layer = self.get_chunk_layer(parent_ids[0])
+            parent_coords_d = {
+                node_id: self.get_chunk_coordinates(node_id) for node_id in parent_ids
+            }
 
-        parent_children_d = {
-            parent_id: np.array([parent_id], dtype=basetypes.NODE_ID)
-            for parent_id in parent_ids
-        }
+            parent_bounding_chunk_ids = defaultdict(lambda: types.empty_1d)
+            parent_layer_mask = {}
+
+            parent_children_d = {
+                parent_id: np.array([parent_id], dtype=basetypes.NODE_ID)
+                for parent_id in parent_ids
+            }
 
         children_layer = parents_layer - 1
         while children_layer >= 2:
+            # with TimeIt("before get children"):
             parent_masked_children_d = {}
             for parent_id, (X, Y, Z) in parent_coords_d.items():
-                chunks = chunk_utils.get_bounding_children_chunks(
-                    self.meta, parents_layer, (X, Y, Z), children_layer
+                coords = chunk_utils.get_bounding_children_chunks(
+                    self.meta, parents_layer, (X, Y, Z), children_layer,
                 )
-                parent_bounding_chunk_ids[parent_id] = np.array(
-                    [
-                        self.get_chunk_id(layer=children_layer, x=x, y=y, z=z)
-                        for (x, y, z) in chunks
-                    ],
-                    dtype=basetypes.CHUNK_ID,
+                chunks_ids = chunk_utils.get_chunk_ids(
+                    self.meta, children_layer, coords
                 )
+                # chunks_ids_1 = np.array(
+                #     [
+                #         self.get_chunk_id(
+                #             layer=children_layer, x=x, y=y, z=z
+                #         )
+                #         for (x, y, z) in coords
+                #     ],
+                #     dtype=basetypes.CHUNK_ID,
+                # )
+                # assert np.all(chunks_ids == chunks_ids_1)
+                parent_bounding_chunk_ids[parent_id] = chunks_ids
                 children = parent_children_d[parent_id]
                 layer_mask = self.get_chunk_layers(children) > children_layer
                 parent_layer_mask[parent_id] = layer_mask
                 parent_masked_children_d[parent_id] = children[layer_mask]
 
+            # with TimeIt("get children"):
             children_ids = np.concatenate(list(parent_masked_children_d.values()))
             child_grand_children_d = self.get_children(children_ids)
+            # with TimeIt("after get children"):
             for parent_id, masked_children in parent_masked_children_d.items():
                 bounding_chunk_ids = parent_bounding_chunk_ids[parent_id]
                 grand_children = [types.empty_1d]

pychunkedgraph/graph/chunks/utils.py
@@ -98,6 +98,23 @@ def get_chunk_id(
     return _compute_chunk_id(meta, layer, x, y, z)
 
 
+def get_chunk_ids(meta, layer: int, coords: np.ndarray):
+    result = np.zeros(len(coords), dtype=np.uint64)
+    s_bits_per_dim = meta.bitmasks[layer]
+
+    layer_offset = 64 - meta.graph_config.LAYER_ID_BITS
+    x_offset = layer_offset - s_bits_per_dim
+    y_offset = x_offset - s_bits_per_dim
+    z_offset = y_offset - s_bits_per_dim
+    coords = np.array(coords, dtype=np.uint64)
+
+    result |= layer << layer_offset
+    result |= coords[:, 0] << x_offset
+    result |= coords[:, 1] << y_offset
+    result |= coords[:, 2] << z_offset
+    return result
+
+
 def get_chunk_ids_from_node_ids(meta, node_ids: Iterable[np.uint64]) -> np.ndarray:
     """ Extract a list of Chunk IDs from a list of Node IDs
     :param node_ids: np.ndarray(dtype=np.uint64)
@@ -168,23 +185,14 @@ def get_bounding_children_chunks(
     x1, y1, z1 = chunk_offset
     x2, y2, z2 = chunk_offset + chunks_count
 
-    # f = lambda range1, range2: product(range(*range1), range(*range2))
     # https://stackoverflow.com/a/35608701/2683367
     f = lambda r1, r2, r3: np.array(np.meshgrid(r1, r2, r3), dtype=int).T.reshape(-1, 3)
-
-    # chunks.extend([np.array([x1, d1, d2]) for d1, d2 in f((y1, y2), (z1, z2))])
-    # chunks.extend([np.array([x2 - 1, d1, d2]) for d1, d2 in f((y1, y2), (z1, z2))])
     chunks.append(f((x1, x2 - 1), range(y1, y2), range(z1, z2)))
-
-    # chunks.extend([np.array([d1, y1, d2]) for d1, d2 in f((x1, x2), (z1, z2))])
-    # chunks.extend([np.array([d1, y2 - 1, d2]) for d1, d2 in f((x1, x2), (z1, z2))])
-    chunks.append(f(range(x1, x2), [y1, y2 - 1], range(z1, z2)))
-
-    # chunks.extend([np.array([d1, d2, z1]) for d1, d2 in f((x1, x2), (y1, y2))])
-    # chunks.extend([np.array([d1, d2, z2 - 1]) for d1, d2 in f((x1, x2), (y1, y2))])
-    chunks.append(f(range(x1, x2), range(y1, y2), [z1, z2 - 1]))
+    chunks.append(f(range(x1, x2), (y1, y2 - 1), range(z1, z2)))
+    chunks.append(f(range(x1, x2), range(y1, y2), (z1, z2 - 1)))
 
     chunks = np.concatenate(chunks)
     mask = np.all(chunks < cg_meta.layer_chunk_bounds[children_layer], axis=1)
     result = chunks[mask]
     return np.unique(result, axis=0) if result.size else result
+