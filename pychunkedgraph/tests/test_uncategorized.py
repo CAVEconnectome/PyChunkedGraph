eab6c15eaed0796aeb189d914a55185115cfb37f
Akhilesh Halageri <akhileshhalageri@gmail.com>
2020-08-17 06:53:01 +0000

    fix: test docs

--- pychunkedgraph/tests/test_uncategorized.py
+++ pychunkedgraph/tests/test_uncategorized.py
@@ -3163,10 +3163,12 @@ class TestGraphLocks:
         │  2  │     │
         └─────┴─────┘
 
-        (1) Try indefinite lock (opid = 1), get indefinite lock
-        (2) Try normal lock (opid = 2), doesn't get the normal lock
-        (3) Try unlock indefinite lock (opid = 1), should unlock indefinite lock
-        (4) Try lock (opid = 2), should get the normal lock
+        (1) Try normal lock (opid = 1), get normal lock
+        (2) Try indefinite lock (opid = 1), get indefinite lock
+        (3) Wait until normal lock expires
+        (4) Try normal lock (opid = 2), doesn't get the normal lock
+        (5) Try unlock indefinite lock (opid = 1), should unlock indefinite lock
+        (6) Try lock (opid = 2), should get the normal lock
         """
 
         # 1. TODO renew lock test when getting indefinite lock
@@ -3197,6 +3199,13 @@ class TestGraphLocks:
         root_id = cg.get_root(to_label(cg, 1, 0, 0, 0, 1))
 
         future_root_ids_d = {root_id: get_future_root_ids(cg, root_id)}
+
+        assert cg.client.lock_roots(
+            root_ids=[root_id],
+            operation_id=operation_id_1,
+            future_root_ids_d=future_root_ids_d,
+        )[0]
+
         assert cg.client.lock_roots_indefinitely(
             root_ids=[root_id],
             operation_id=operation_id_1,
@@ -3221,6 +3230,64 @@ class TestGraphLocks:
             operation_id=operation_id_2,
             future_root_ids_d=future_root_ids_d,
         )[0]
+
+    # TODO fixme: this scenario can't be tested like this
+    # @pytest.mark.timeout(30)
+    # def test_normal_lock_expiration(self, gen_graph):
+    #     """
+    #     No connection between 1, 2 and 3
+    #     ┌─────┬─────┐
+    #     │  A¹ │  B¹ │
+    #     │  1  │  3  │
+    #     │  2  │     │
+    #     └─────┴─────┘
+
+    #     (1) Try normal lock (opid = 1), get normal lock
+    #     (2) Wait until normal lock expires 
+    #     (3) Try indefinite lock (opid = 1), doesn't get the indefinite lock
+    #     """
+
+    #     cg = gen_graph(n_layers=3)
+
+    #     # Preparation: Build Chunk A
+    #     fake_timestamp = datetime.utcnow() - timedelta(days=10)
+    #     create_chunk(
+    #         cg,
+    #         vertices=[to_label(cg, 1, 0, 0, 0, 1), to_label(cg, 1, 0, 0, 0, 2)],
+    #         edges=[],
+    #         timestamp=fake_timestamp,
+    #     )
+
+    #     # Preparation: Build Chunk B
+    #     create_chunk(
+    #         cg,
+    #         vertices=[to_label(cg, 1, 1, 0, 0, 1)],
+    #         edges=[],
+    #         timestamp=fake_timestamp,
+    #     )
+
+    #     add_layer(
+    #         cg, 3, [0, 0, 0], time_stamp=fake_timestamp, n_threads=1,
+    #     )
+
+    #     operation_id_1 = cg.id_client.create_operation_id()
+    #     root_id = cg.get_root(to_label(cg, 1, 0, 0, 0, 1))
+
+    #     future_root_ids_d = {root_id: get_future_root_ids(cg, root_id)}
+
+    #     assert cg.client.lock_roots(
+    #         root_ids=[root_id],
+    #         operation_id=operation_id_1,
+    #         future_root_ids_d=future_root_ids_d,
+    #     )[0]
+
+    #     sleep(cg.meta.graph_config.ROOT_LOCK_EXPIRY.total_seconds()+1)
+
+    #     assert not cg.client.lock_roots_indefinitely(
+    #         root_ids=[root_id],
+    #         operation_id=operation_id_1,
+    #         future_root_ids_d=future_root_ids_d,
+    #     )[0]
 
 
 # class MockChunkedGraph:
