eee10ad833dcc515c5886f0d853ecf59fd5db653
Manuel Castro <macastro@princeton.edu>
2021-01-03 20:53:03 -0500

    add find_path coarse precision (#256)

--- pychunkedgraph/app/segmentation/common.py
+++ pychunkedgraph/app/segmentation/common.py
@@ -1024,7 +1024,7 @@ def handle_split_preview(table_id):
 ### FIND PATH --------------------------------------------------------------
 
 
-def handle_find_path(table_id):
+def handle_find_path(table_id, precision_mode):
     current_app.table_id = table_id
     user_id = str(g.auth_user["id"])
     current_app.user_id = user_id
@@ -1059,13 +1059,20 @@ def handle_find_path(table_id):
     target_l2_id = cg.get_parent(target_supervoxel_id)
 
     l2_path = analysis.find_l2_shortest_path(cg, source_l2_id, target_l2_id)
-    centroids, failed_l2_ids = analysis.compute_mesh_centroids_of_l2_ids(cg, l2_path, flatten=True)
-
-    return {
-        "centroids_list": centroids,
-        "failed_l2_ids": failed_l2_ids,
-        "l2_path": l2_path
-    }
+    if precision_mode:
+        centroids, failed_l2_ids = analysis.compute_mesh_centroids_of_l2_ids(cg, l2_path, flatten=True)
+        return {
+            "centroids_list": centroids,
+            "failed_l2_ids": failed_l2_ids,
+            "l2_path": l2_path
+        }
+    else:
+        centroids = analysis.compute_rough_coordinate_path(cg, l2_path)
+        return {
+            "centroids_list": centroids,
+            "failed_l2_ids": [],
+            "l2_path": l2_path
+        }
 
 ### GET_LAYER2_SUBGRAPH
 def handle_get_layer2_graph(table_id, node_id):
