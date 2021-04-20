ed2243652c801d3b567ff69c270fb9194ab60b17
Manuel Castro <manuel.a.castro1992@gmail.com>
2021-04-20 09:42:43 -0400

    fix find path precision mode

--- pychunkedgraph/meshing/mesh_analysis.py
+++ pychunkedgraph/meshing/mesh_analysis.py
@@ -1,4 +1,4 @@
-from cloudvolume import CloudVolume, Storage
+from cloudvolume import CloudVolume
 import numpy as np
 import os
 from pychunkedgraph.meshing import meshgen, meshgen_utils
@@ -68,43 +68,30 @@ def compute_mesh_centroids_of_l2_ids(cg, l2_ids, flatten=False):
         mesh_dir=cv_sharded_mesh_dir,
         info=meshgen_utils.get_json_info(cg),
     )
-    fragments_to_fetch = [
-        f"{l2_id}:0:{meshgen_utils.get_chunk_bbox_str(cg, cg.get_chunk_id(l2_id))}"
-        for l2_id in l2_ids
-    ]
+    meshes = cv.mesh.get_meshes_on_bypass(l2_ids, allow_missing=True)
     if flatten:
         centroids_with_chunk_boundary_points = []
     else:
         centroids_with_chunk_boundary_points = {}
     last_l2_id = None
-    failed_l2_ids = []
-    with Storage(cv_unsharded_mesh_path) as storage:
-        files_contents = storage.get_files(fragments_to_fetch)
-        fragment_map = meshgen.get_missing_initial_meshes(cv, files_contents)
-        for i in range(len(fragments_to_fetch)):
-            fragment_to_fetch = fragments_to_fetch[i]
-            l2_id = l2_ids[i]
-            try:
-                fragment = fragment_map[fragment_to_fetch]
-                if fragment["content"] is not None and fragment["error"] is None:
-                    if "skip_decode" in fragment:
-                        vertices = fragment["content"].vertices
-                    else:
-                        mesh = meshgen.decode_draco_mesh_buffer(fragment["content"])
-                        vertices = mesh["vertices"]
-                    if flatten:
-                        centroids_with_chunk_boundary_points.extend(
-                            compute_centroid_with_chunk_boundary(
-                                cg, vertices, l2_id, last_l2_id
-                            )
-                        )
-                    else:
-                        centroids_with_chunk_boundary_points[
-                            l2_id
-                        ] = compute_centroid_with_chunk_boundary(
-                            cg, vertices, l2_id, last_l2_id
-                        )
-            except:
-                failed_l2_ids.append(l2_id)
-            last_l2_id = l2_id
-    return centroids_with_chunk_boundary_points, failed_l2_ids
+    missing_l2_ids = []
+    for l2_id_i in l2_ids:
+        l2_id = int(l2_id_i)
+        try:
+            l2_mesh_vertices = meshes[l2_id].vertices
+            if flatten:
+                centroids_with_chunk_boundary_points.extend(
+                    compute_centroid_with_chunk_boundary(
+                        cg, l2_mesh_vertices, l2_id, last_l2_id
+                    )
+                )
+            else:
+                centroids_with_chunk_boundary_points[
+                    l2_id
+                ] = compute_centroid_with_chunk_boundary(
+                    cg, l2_mesh_vertices, l2_id, last_l2_id
+                )
+        except:
+            missing_l2_ids.append(l2_id)
+        last_l2_id = l2_id
+    return centroids_with_chunk_boundary_points, missing_l2_ids
