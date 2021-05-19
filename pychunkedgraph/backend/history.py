8ef9fab36db81214417917392ba30396c268ccab
sdorkenw <sven.dorkenwald@googlemail.com>
2021-05-19 12:39:19 -0700

    typo

--- pychunkedgraph/backend/history.py
+++ pychunkedgraph/backend/history.py
@@ -105,32 +105,45 @@ class History:
             )
         return filtered_tabular_changelogs
 
-    def collect_edited_sv_ids(self, root_id=None): 
+    def collect_edited_sv_ids(self, root_id=None):
         if root_id is None:
             operation_ids = self.past_operation_ids()
         else:
             assert root_id in self.root_ids
             operation_ids = self.past_operation_ids(root_id=root_id)
-            
+
         edited_sv_ids = []
         for operation_id in operation_ids:
             edited_sv_ids.extend(self.log_entry(operation_id).edges_failsafe)
 
         return fastremap.unique(np.array(edited_sv_ids))
-                
+
     def _build_tabular_changelogs(self):
         tabular_changelogs = {}
         all_user_ids = []
         for root_id in self.root_ids:
-            root_ts = self.cg.read_node_id_row(root_id)[column_keys.Hierarchy.Child][0].timestamp
+            root_ts = self.cg.read_node_id_row(root_id)[column_keys.Hierarchy.Child][
+                0
+            ].timestamp
             edited_sv_ids = self.collect_edited_sv_ids(root_id=root_id)
-            current_root_id_lookup_vec = np.vectorize(dict(zip(edited_sv_ids,
-                                                                self.cg.get_roots(edited_sv_ids,
-                                                                                  time_stamp=root_ts))).get)
-            original_root_id_lookup_vec = np.vectorize(dict(zip(edited_sv_ids,
-                                                                self.cg.get_roots(edited_sv_ids,
-                                                                                  time_stamp=self.cg.get_earliest_timestamp()))).get)
-            
+            current_root_id_lookup_vec = np.vectorize(
+                dict(
+                    zip(
+                        edited_sv_ids,
+                        self.cg.get_roots(edited_sv_ids, time_stamp=root_ts),
+                    )
+                ).get
+            )
+            original_root_id_lookup_vec = np.vectorize(
+                dict(
+                    zip(
+                        edited_sv_ids,
+                        self.cg.get_roots(
+                            edited_sv_ids, time_stamp=self.cg.get_earliest_timestamp()
+                        ),
+                    )
+                ).get
+            )
 
             is_merge_list = []
             is_in_neuron_list = []
@@ -150,7 +163,7 @@ class History:
                 user_list.append(entry.user_id)
 
                 sv_ids_original_root = original_root_id_lookup_vec(entry.edges_failsafe)
-                sv_ids_current_root = current_root_id_lookup_vec(entry.edges_failsafe)Z
+                sv_ids_current_root = current_root_id_lookup_vec(entry.edges_failsafe)
                 before_ids = list(self.operation_id_root_id_dict[operation_id])
                 after_root_ids_list.append(
                     list(self.lineage_graph.neighbors(before_ids[0]))
