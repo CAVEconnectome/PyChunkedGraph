151c5a174c2396efd8cf30859ec23c3e18c3c481
Akhilesh Halageri <akhileshhalageri@gmail.com>
2022-03-13 19:47:42 +0000

    fix: convert print statements to logging

--- pychunkedgraph/__init__.py
+++ pychunkedgraph/__init__.py
@@ -1,1 +1,12 @@
-__version__ = '1.19.0'
+from os import environ
+from logging import getLogger
+
+LOGGER_NAME = "pcg"
+INFO_PRIORITY = 25
+
+__version__ = "1.19.0"
+
+
+# create logger instance
+logger = getLogger(LOGGER_NAME)
+logger.setLevel(environ.get("PCG_LOG_LEVEL", INFO_PRIORITY))
--- run_dev.py
+++ run_dev.py
@@ -1,7 +1,9 @@
-import sys
-from werkzeug.serving import WSGIRequestHandler
 import os
+import sys
+from logging import getLogger
 
+from werkzeug.serving import WSGIRequestHandler
+from pychunkedgraph import LOGGER_NAME
 from pychunkedgraph.app import create_app
 
 app = create_app()
@@ -10,15 +12,14 @@ if __name__ == '__main__':
 
     assert len(sys.argv) == 2
     HOME = os.path.expanduser("~")
-
     port = int(sys.argv[1])
 
     # Set HTTP protocol
     WSGIRequestHandler.protocol_version = "HTTP/1.1"
     # WSGIRequestHandler.protocol_version = "HTTP/2.0"
 
-    print("Table: %s; Port: %d" %
-          (app.config['CHUNKGRAPH_TABLE_ID'], port))
+    logger = getLogger(LOGGER_NAME)
+    logger.debug(app.config["PCG_GRAPH_IDS"])
 
     app.run(host='0.0.0.0',
             port=port,
