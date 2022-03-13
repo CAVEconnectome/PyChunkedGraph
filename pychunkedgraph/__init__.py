6498b575281822eac03f33d3eeab64fd14a1c29c
Akhilesh Halageri <akhileshhalageri@gmail.com>
2022-03-13 23:24:45 +0000

    fix: str to int

--- pychunkedgraph/__init__.py
+++ pychunkedgraph/__init__.py
@@ -9,5 +9,5 @@ __version__ = "1.19.0"
 
 # create logger instance
 logger = getLogger(LOGGER_NAME)
-logger.setLevel(environ.get("PCG_LOG_LEVEL", INFO_PRIORITY))
+logger.setLevel(int(environ.get("PCG_LOG_LEVEL", INFO_PRIORITY)))
 pcg_logger = logger
