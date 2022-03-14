adcf3e2497d4556d9d3cbacc89db00e37396afcb
Akhilesh Halageri <akhileshhalageri@gmail.com>
2022-03-14 16:25:50 +0000

    fix: log to stdout

--- pychunkedgraph/__init__.py
+++ pychunkedgraph/__init__.py
@@ -1,5 +1,6 @@
 from os import environ
 from logging import getLogger
+from logging import basicConfig
 
 LOGGER_NAME = "pcg"
 INFO_PRIORITY = 25
@@ -9,5 +10,13 @@ __version__ = "1.19.0"
 
 # create logger instance
 logger = getLogger(LOGGER_NAME)
-logger.setLevel(int(environ.get("PCG_LOG_LEVEL", INFO_PRIORITY)))
+log_level = int(environ.get("PCG_LOG_LEVEL", INFO_PRIORITY))
+
+# Set the root logger to write to stdout.
+basicConfig(
+    level=log_level,
+    format="%(asctime)s %(message)s",
+    datefmt="%m/%d/%Y %I:%M:%S %p",
+)
+logger.setLevel(log_level)
 pcg_logger = logger
