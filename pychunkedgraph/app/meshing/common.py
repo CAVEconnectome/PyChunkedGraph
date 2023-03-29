e892cfdcf0d28b4c1fab7d6a874ad0566b423a29
Akhilesh Halageri <akhileshhalageri@gmail.com>
2023-03-29 16:05:09 +0000

    feat: multiscale manifest endpoint

--- pychunkedgraph/app/meshing/common.py
+++ pychunkedgraph/app/meshing/common.py
@@ -16,6 +16,10 @@ from pychunkedgraph.meshing import meshgen
 from pychunkedgraph.meshing.manifest import get_highest_child_nodes_with_meshes
 from pychunkedgraph.meshing.manifest import get_children_before_start_layer
 from pychunkedgraph.meshing.manifest import ManifestCache
+from pychunkedgraph.meshing.manifest import speculative_manifest_sharded
+from pychunkedgraph.meshing.manifest.multiscale import (
+    get_manifest as get_multiscale_manifest,
+)
 
 
 __meshing_url_prefix__ = os.environ.get("MESHING_URL_PREFIX", "meshing")
@@ -54,7 +58,7 @@ def handle_valid_frags(table_id, node_id):
 ## MANIFEST --------------------------------------------------------------------
 
 
-def handle_get_manifest(table_id, node_id):
+def handle_get_manifest(table_id, node_id, multiscale=False):
     current_app.request_type = "manifest"
     current_app.table_id = table_id
 
@@ -84,6 +88,7 @@ def handle_get_manifest(table_id, node_id):
     args = (
         node_id,
         verify,
+        multiscale,
         return_seg_ids,
         prepend_seg_ids,
         start_layer,
@@ -95,11 +100,10 @@ def handle_get_manifest(table_id, node_id):
 
 
 def manifest_response(cg, args):
-    from pychunkedgraph.meshing.manifest import speculative_manifest_sharded
-
     (
         node_id,
         verify,
+        multiscale,
         return_seg_ids,
         prepend_seg_ids,
         start_layer,
@@ -109,11 +113,14 @@ def manifest_response(cg, args):
     ) = args
     resp = {}
     seg_ids = []
+    node_id = np.uint64(node_id)
     if not verify:
         seg_ids, resp["fragments"] = speculative_manifest_sharded(
             cg, node_id, start_layer=start_layer, bounding_box=bounding_box
         )
-
+    elif multiscale is True:
+        seg_ids, response = get_multiscale_manifest(cg, node_id)
+        resp.update(response)
     else:
         seg_ids, resp["fragments"] = get_highest_child_nodes_with_meshes(
             cg,
