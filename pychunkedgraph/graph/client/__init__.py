ceec0e3d23d98036cc0e01fbc0311748c2d83687
Dmitri Dmitrienko <ddmitrie@amazon.com>
2023-06-16 18:39:04 -0400

    Set default client_info.TYPE from the BACKEND_CLIENT_TYPE environment variable; Import backend client packages only if corresponding backend type is selected

--- pychunkedgraph/graph/client/__init__.py
+++ pychunkedgraph/graph/client/__init__.py
@@ -17,9 +17,9 @@ making it possible to use any unique features these solutions may provide.
 Please see `base.py` for more details.
 """
 
+from os import environ
 from collections import namedtuple
 
-from .bigtable.client import Client as BigTableClient
 
 
 _backend_clientinfo_fields = ("TYPE", "CONFIG")
@@ -29,16 +29,27 @@ BackendClientInfo = namedtuple(
     _backend_clientinfo_fields,
     defaults=_backend_clientinfo_defaults,
 )
-
+DEFAULT_BACKEND_TYPE = "GCP.BIGTABLE"
 
 def get_default_client_info():
     """
     Load client from env variables.
     """
-
-    # TODO make dynamic after multiple platform support is added
-    from .bigtable import get_client_info as get_bigtable_client_info
-
-    return BackendClientInfo(
-        CONFIG=get_bigtable_client_info(admin=True, read_only=False)
-    )
+    backend_type_env = environ.get("BACKEND_CLIENT_TYPE", DEFAULT_BACKEND_TYPE)
+    if backend_type_env == "GCP.BIGTABLE":
+        from .bigtable.client import Client as BigTableClient
+        from .bigtable import get_client_info as get_bigtable_client_info
+        client_info = BackendClientInfo(
+            TYPE=BigTableClient,
+            CONFIG=get_bigtable_client_info(admin=True, read_only=False)
+        )
+    elif backend_type_env == "AMAZON.DYNAMODB":
+        from .amazon.dynamodb.client import Client as AmazonDynamoDbClient
+        from .amazon.dynamodb import get_client_info as get_amazon_dynamodb_client_info
+        client_info = BackendClientInfo(
+            TYPE=AmazonDynamoDbClient,
+            CONFIG=get_amazon_dynamodb_client_info(admin=True, read_only=False)
+        )
+    else:
+        raise TypeError(f"Client backend {str(backend_type_env)} is not supported")
+    return client_info
