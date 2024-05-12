0644c9c891c00481df7af075e7cfe88622c482f3
Akhilesh Halageri <akhileshhalageri@gmail.com>
2024-05-12 10:35:48 -0500

    V3 migration (#484)
    * feat: convert edges to ocdbt
    
    * feat: worker function to convert edges to ocdbt
    
    * feat: ocdbt option, consolidate ingest cli
    
    * fix(ingest): move fn to utils
    
    * fix(ingest): move ocdbt setup to a fn
    
    * add tensorstore req, fix build kaniko cache
    
    * feat: copy fake_edges to column family 4
    
    * feat: upgrade atomic chunks
    
    * fix: rename abstract module to parent
    
    * feat: upgrade higher layers, docs
    
    * feat: upgrade cli, move common fns to utils
    
    * add copy_fake_edges in upgrade fn
    
    * handle earliest_timestamp, add test flag to upgrade
    
    * fix: fake_edges serialize np.uint64
    
    * add get_operation method, fix timestamp in repair, check for parent
    
    * check for l2 ids invalidated by edit retries
    
    * remove unnecessary parent assert
    
    * remove unused vars
    
    * ignore invalid ids, assert parent after earliest_ts
    
    * check for ids invalidated by retries in higher layers
    
    * parallelize update_cross_edges
    
    * overwrite graph version, create col family 4
    
    * improve status print formatting
    
    * remove ununsed code, consolidate small common module
    
    * efficient check for chunks not done
    
    * check for empty chunks, use get_parents
    
    * efficient get_edit_ts call by batching all children
    
    * reduce earliest_ts calls
    
    * combine bigtable calls, use numpy unique
    
    * add completion rate command
    
    * fix: ignore children without cross edges
    
    * add span option to rate calculation
    
    * reduce mem usage with global vars
    
    * optimize cross edge reading
    
    * use existing layer var
    
    * limit cx edge reading above given layer
    
    * fix: read for earliest_ts check only if true
    
    * filter cross edges fn with timestamps
    
    * remove git from dockerignore, print stats
    
    * shuffle for better distribution of ids
    
    * fix: use different var name for layer
    
    * increase bigtable read timeout
    
    * add message with assert
    
    * fix: make span option int
    
    * handle skipped connections
    
    * fix: read cross edges at layer >= node_layer
    
    * handle another case of skipped nodes
    
    * check for unique parent count
    
    * update graph_id in meta
    
    * uncomment line
    
    * make repair easier to use
    
    * add sanity check for edits
    
    * add sanity check for each layer
    
    * add layers flag for cx edges
    
    * use better names for functions and vars, update types, fix docs

--- pychunkedgraph/ingest/__init__.py
+++ pychunkedgraph/ingest/__init__.py
@@ -1,32 +1,16 @@
+import logging
 from collections import namedtuple
 
-
-_cluster_ingest_config_fields = (
-    "ATOMIC_Q_NAME",
-    "ATOMIC_Q_LIMIT",
-    "ATOMIC_Q_INTERVAL",
-)
-_cluster_ingest_defaults = (
-    "l2",
-    100000,
-    120,
-)
-ClusterIngestConfig = namedtuple(
-    "ClusterIngestConfig",
-    _cluster_ingest_config_fields,
-    defaults=_cluster_ingest_defaults,
-)
-
+logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
 
 _ingestconfig_fields = (
-    "CLUSTER",  # cluster config
     "AGGLOMERATION",
     "WATERSHED",
     "USE_RAW_EDGES",
     "USE_RAW_COMPONENTS",
     "TEST_RUN",
 )
-_ingestconfig_defaults = (None, None, None, False, False, False)
+_ingestconfig_defaults = (None, None, False, False, False)
 IngestConfig = namedtuple(
     "IngestConfig", _ingestconfig_fields, defaults=_ingestconfig_defaults
 )
--- pychunkedgraph/repair/edits.py
+++ pychunkedgraph/repair/edits.py
@@ -1,35 +1,31 @@
 # pylint: disable=protected-access,missing-function-docstring,invalid-name,wrong-import-position
 
-from os import environ
 from datetime import timedelta
-
-environ["BIGTABLE_PROJECT"] = "<>"
-environ["BIGTABLE_INSTANCE"] = "<>"
-environ["GOOGLE_APPLICATION_CREDENTIALS"] = "<path>"
 
 from pychunkedgraph.graph import ChunkedGraph
 from pychunkedgraph.graph.attributes import Concurrency
 from pychunkedgraph.graph.operation import GraphEditOperation
 
 
-def repair_operation(cg, log_d, operation_id):
+def repair_operation(cg: ChunkedGraph, operation_id: int, unlock: bool = False):
     operation = GraphEditOperation.from_operation_id(
         cg, operation_id, multicut_as_split=False, privileged_mode=True
     )
-    ts = log_d["timestamp"]
+    _, ts = cg.client.read_log_entry(operation_id)
     result = operation.execute(
         operation_id=operation_id,
-        parent_ts=ts - timedelta(seconds=0.1),
-        override_ts=ts + timedelta(microseconds=(ts.microsecond % 1000) + 10),
+        parent_ts=ts - timedelta(milliseconds=100),
+        override_ts=ts + timedelta(milliseconds=10),
     )
     old_roots = operation._update_root_ids()
     print("roots", old_roots, result.new_root_ids)
     print("result op ID", result.operation_id)
     print("result L2 IDs", result.new_lvl2_ids)
 
-    for root_ in old_roots:
-        cg.client.unlock_root(root_, result.operation_id)
-        cg.client.unlock_indefinitely_locked_root(root_, result.operation_id)
+    if unlock:
+        for root_ in old_roots:
+            cg.client.unlock_root(root_, result.operation_id)
+            cg.client.unlock_indefinitely_locked_root(root_, result.operation_id)
 
 
 if __name__ == "__main__":
@@ -44,8 +40,6 @@ if __name__ == "__main__":
             op_ids_to_retry.append(locked_op)
             print(f"{node_id} indefinitely locked by op {locked_op}")
     print(f"total to retry: {len(op_ids_to_retry)}")
-
-    logs = cg.client.read_log_entries(op_ids_to_retry)
-    for op_id, log in logs.items():
+    for op_id in op_ids_to_retry:
         print(f"repairing {op_id}")
-        repair_operation(cg, log, op_id)
+        repair_operation(cg, op_id)
