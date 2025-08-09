33c24d4370dfd0abf852e823a93d437a448f9972
Akhilesh Halageri <akhileshhalageri@gmail.com>
2025-08-09 17:17:12 +0000

    fix(upgrade): use start and end timestamps to filter out irreleveant timestamps

--- pychunkedgraph/ingest/upgrade/atomic_layer.py
+++ pychunkedgraph/ingest/upgrade/atomic_layer.py
@@ -1,18 +1,18 @@
 # pylint: disable=invalid-name, missing-docstring, c-extension-no-member
 
-from datetime import timedelta
-
 import fastremap
 import numpy as np
 from pychunkedgraph.graph import ChunkedGraph
-from pychunkedgraph.graph.attributes import Connectivity
+from pychunkedgraph.graph.attributes import Connectivity, Hierarchy
 from pychunkedgraph.graph.utils import serializers
 
-from .utils import exists_as_parent, get_parent_timestamps
+from .utils import exists_as_parent, get_end_timestamps, get_parent_timestamps
+
+CHILDREN = {}
 
 
 def update_cross_edges(
-    cg: ChunkedGraph, node, cx_edges_d: dict, node_ts, timestamps: set, earliest_ts
+    cg: ChunkedGraph, node, cx_edges_d: dict, node_ts, node_end_ts, timestamps: set
 ) -> list:
     """
     Helper function to update a single L2 ID.
@@ -27,13 +27,15 @@ def update_cross_edges(
         assert not exists_as_parent(cg, node, edges[:, 0])
         return rows
 
-    partner_parent_ts_d = get_parent_timestamps(cg, edges[:, 1])
+    partner_parent_ts_d = get_parent_timestamps(cg, np.unique(edges[:, 1]))
     for v in partner_parent_ts_d.values():
         timestamps.update(v)
 
     for ts in sorted(timestamps):
-        if ts < earliest_ts:
-            ts = earliest_ts
+        if ts < node_ts:
+            continue
+        if ts > node_end_ts:
+            break
         val_dict = {}
         svs = edges[:, 1]
         parents = cg.get_parents(svs, time_stamp=ts)
@@ -51,21 +53,20 @@ def update_cross_edges(
     return rows
 
 
-def update_nodes(cg: ChunkedGraph, nodes) -> list:
-    nodes_ts = cg.get_node_timestamps(nodes, return_numpy=False, normalize=True)
-    earliest_ts = cg.get_earliest_timestamp()
+def update_nodes(cg: ChunkedGraph, nodes, nodes_ts) -> list:
+    end_timestamps = get_end_timestamps(cg, nodes, nodes_ts, CHILDREN)
     timestamps_d = get_parent_timestamps(cg, nodes)
     cx_edges_d = cg.get_atomic_cross_edges(nodes)
     rows = []
-    for node, node_ts in zip(nodes, nodes_ts):
+    for node, node_ts, end_ts in zip(nodes, nodes_ts, end_timestamps):
         if cg.get_parent(node) is None:
-            # invalid id caused by failed ingest task
+            # invalid id caused by failed ingest task / edits
             continue
         _cx_edges_d = cx_edges_d.get(node, {})
         if not _cx_edges_d:
             continue
         _rows = update_cross_edges(
-            cg, node, _cx_edges_d, node_ts, timestamps_d[node], earliest_ts
+            cg, node, _cx_edges_d, node_ts, end_ts, timestamps_d[node]
         )
         rows.extend(_rows)
     return rows
@@ -76,10 +77,26 @@ def update_chunk(cg: ChunkedGraph, chunk_coords: list[int], layer: int = 2):
     Iterate over all L2 IDs in a chunk and update their cross chunk edges,
     within the periods they were valid/active.
     """
+    global CHILDREN
+
     x, y, z = chunk_coords
     chunk_id = cg.get_chunk_id(layer=layer, x=x, y=y, z=z)
     cg.copy_fake_edges(chunk_id)
     rr = cg.range_read_chunk(chunk_id)
-    nodes = list(rr.keys())
-    rows = update_nodes(cg, nodes)
+
+    nodes = []
+    nodes_ts = []
+    earliest_ts = cg.get_earliest_timestamp()
+    for k, v in rr.items():
+        nodes.append(k)
+        CHILDREN[k] = v[Hierarchy.Child][0].value
+        ts = v[Hierarchy.Child][0].timestamp
+        nodes_ts.append(earliest_ts if ts < earliest_ts else ts)
+
+    if len(nodes):
+        assert len(CHILDREN) > 0, (nodes, CHILDREN)
+    else:
+        return
+
+    rows = update_nodes(cg, nodes, nodes_ts)
     cg.client.write(rows)
